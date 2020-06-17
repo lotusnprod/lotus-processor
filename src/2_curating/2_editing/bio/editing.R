e9503e3f18e83445a534f1a436f8005b50980545
Jonathan Bisson <bjo@uic.edu>
2020-06-17 15:18:23 -0500

    [Clean] Move gnfinder code closer to bio

--- src/2_curating/2_editing/bio/editing.R
+++ src/2_curating/2_editing/bio/editing.R
@@ -6,6 +6,61 @@ source("paths.R")
 # loading functions
 source("functions/bio.R")
 
+gnfinder_cleaning <- function(num, organismCol) {
+  if (organismCol == "organismOriginal") {
+    inpath_organism_f <- paste(
+      pathOriginalOrganismDistinct,
+      "originalOrganismGnfinderUntil_",
+      num,
+      ".tsv",
+      sep = "")
+
+    inpath_gnfinder_f <-
+      paste(
+        pathCleanedOrganismOriginalDirJson,
+        "originalOrganismGnfinderUntil_",
+        num,
+        ".json",
+        sep = "")
+  }
+
+  if (organismCol == "organismInterim") {
+    inpath_organism_f <- paste(
+      pathTranslatedOrganismDistinct,
+      "translatedOrganismGnfinderUntil_",
+      num,
+      ".tsv",
+      sep = "")
+
+    inpath_gnfinder_f <-
+      paste(
+        pathCleanedOrganismTranslatedDirJson,
+        "translatedOrganismGnfinderUntil_",
+        num,
+        ".json",
+        sep = "")
+   }
+
+  gnfound <- data.frame(fromJSON(txt = inpath_gnfinder_f,
+                                 simplifyDataFrame = TRUE))
+
+  data_bio <- read_delim(
+    file = inpath_organism_f,
+    delim = "\t",
+    escape_double = FALSE,
+    trim_ws = FALSE
+  ) %>%
+    mutate_all(as.character)
+
+  data_bio <- data_bio[!is.na(data_bio[, organismCol]),]
+
+  data_bio_clean <- biocleaning(x = gnfound,
+                                y = data_bio,
+                                organismCol = organismCol)
+
+  return(data_bio_clean)
+}
+
 # cleaning original organism
 ###COMMENT### temporary, will write path accordingly if validated that way
 source("2_curating/2_editing/bio/subscripts/1_cleaningOriginalOrganism.R")
