e7f8f5b3dc60fa7fb029903c0f68d0730ed28612
Jonathan Bisson <bjo@uic.edu>
2020-06-17 11:52:41 -0500

    refactor(db): improve alkamid scraper and makefile

--- src/1_gathering/db/alkamid/scraping.R
+++ src/1_gathering/db/alkamid/scraping.R
@@ -3,8 +3,15 @@
 # loading paths
 source("paths.R")
 
-# loading functions
-source("functions.R")
+library(dplyr)
+library(pbmcapply)
+library(parallel)
+library(data.table)
+library(splitstackshape) # provides cSplit
+library(rvest)  # provides read_html
+
+# get paths
+database <- databases$get("alkamid")
 
 url <- 'http://alkamid.ugent.be/molecule.php?ID='
 
@@ -12,93 +19,39 @@ X <- (1:439)
 
 getalkamid <- function(X)
 {
-  tryCatch({
-    cd_id <- X
-    url_id <- paste(url, cd_id, "&typegroup=genusgroup")
-    url_id <- gsub("\\s", "", url_id)
-    df1 <- read_html(url_id) %>%
-      html_node("body") %>%
-      html_node(xpath = "/html/body/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div/div/div/div/div") %>%
-      html_text()
-  },
-  error = function(e) {
-    NA
-  })
+  cd_id <- X
+  url_id <- paste(url, cd_id, "&typegroup=genusgroup")
+  url_id <- gsub("\\s", "", url_id)
+  html <- read_html(url_id) %>% html_node("body")
+  data <- html %>% html_node(xpath = "/html/body/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div/div/div/div/div") %>% html_text()
+  ref <- html %>%
+    html_node(xpath = "/html/body/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div/div/div/div/div/div[8]/div[2]/table") %>%
+    html_table() %>%
+    mutate_all(as.character)
+  list(data=data, ref=ref)
 }
 
-getalkamid_ref <- function(X)
-{
-  tryCatch({
-    cd_id <- X
-    url_id <- paste(url, cd_id, "&typegroup=genusgroup")
-    url_id <- gsub("\\s", "", url_id)
-    df1 <- read_html(url_id) %>%
-      html_node("body") %>%
-      html_node(xpath = "/html/body/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div/div/div/div/div/div[8]/div[2]/table") %>%
-      html_table() %>%
-      mutate_all(as.character)
-  },
-  error = function(e) {
-    NA
-  })
-}
-
-ALKAMID <- invisible(
+extracted_elements <- invisible(
   pbmclapply(
     FUN = getalkamid,
     X = X,
-    mc.preschedule = TRUE,
-    mc.set.seed = TRUE,
-    mc.silent = TRUE,
-    mc.cores = (parallel::detectCores() - 2),
+    mc.silent = FALSE,
+    mc.cores = 8,
     mc.cleanup = TRUE,
     mc.allow.recursive = TRUE
   )
-) %>%
+)
+
+ALKAMID = lapply(extracted_elements, function (x) { x$data }) %>%
   as.data.table() %>%
   t() %>%
   cSplit("V1", "\n")
 
-ALKAMID_REF <- invisible(
-  pbmclapply(
-    FUN = getalkamid_ref,
-    X = X,
-    mc.preschedule = TRUE,
-    mc.set.seed = TRUE,
-    mc.silent = TRUE,
-    mc.cores = (parallel::detectCores() - 2),
-    mc.cleanup = TRUE,
-    mc.allow.recursive = TRUE
-  )
-)
-
+ALKAMID_REF = lapply(extracted_elements, function (x) { x$ref })
 ALKAMID_REF_2 <- ALKAMID_REF[!is.na(ALKAMID_REF)]
-
 ALKAMID_REF_3 <- bind_rows(ALKAMID_REF_2, .id = "entry_id")
 
-# exporting
-write.table(
-  x = ALKAMID,
-  file = gzfile(
-    description = pathDataExternalDbSourceAlkamidOriginal,
-    compression = 9,
-    encoding = "UTF-8"
-  ),
-  row.names = FALSE,
-  quote = FALSE,
-  sep = "\t",
-  fileEncoding = "UTF-8"
-)
+## # exporting
 
-write.table(
-  x = ALKAMID_REF_3,
-  file = gzfile(
-    description  = pathDataExternalDbSourceAlkamidRef,
-    compression = 9,
-    encoding = "UTF-8"
-  ),
-  row.names = FALSE,
-  quote = FALSE,
-  sep = "\t",
-  fileEncoding = "UTF-8"
-)
+database$writeFile(database$sourceFiles$tsv, ALKAMID)
+database$writeFile(database$sourceFiles$tsvRef, ALKAMID_REF_3)
