e919e17a22527323682d829481fa77766b298336
Pierre-Marie ALLARD <allardp@FARMA-NONMEM.epgl.unige.local>
2020-06-30 14:40:19 +0200

    feat(auth): add sys arg to parse input and output files

--- src/2_curating/2_editing/chemo/subscripts/1_translating/smiles_ext.py
+++ src/2_curating/2_editing/chemo/subscripts/1_translating/smiles_ext.py
@@ -4,13 +4,30 @@
 # # RDKIT to generate InChIs
 
 # loading packages
-
+import sys
 from rdkit import Chem
 import pandas as pd
 import gzip
 from rdkit.Chem import PandasTools
 from rdkit.Chem import Descriptors
 import numpy as np
+
+# here we deine input, outputs and other variables as sys arguments
+try:
+    input_file_path = sys.argv[1]
+    ouput_file_path = sys.argv[2]
+    smiles_column_header = sys.argv[3]
+
+    print('Parsing gziped tab separated file'
+          + input_file_path 
+          + 'with column: '
+          + smiles_column_header
+          + 'as SMILES column.'
+          + 'Proceeding to the the ROMol object conversion and returning the InChI in file :'
+          + ouput_file_path)
+except:
+    print('Please add input and output file path as first and second argument and SMILES column header as third argument.')
+
 
 # loading data
 myZip = gzip.open('../data/interim/tables_min/0_original/smiles.tsv.zip')
