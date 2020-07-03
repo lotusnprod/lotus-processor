51354504d619e4fe01576717f0171cf3cc1eb4a9
Adafede <adriano.rutz@unige.ch>
2020-07-03 11:36:01 +0200

    feat(db): standardize nubbe database scraping process

---
+++ src/1_gathering/db/nubbe/standardizing.R
@@ -0,0 +1,152 @@
+# title: "Nubbe cleaneR"
+
+# loading paths
+source("paths.R")
+source("functions/helpers.R")
+source("functions/parallel.R")
+source("functions/standardizing.R")
+
+library(dplyr)
+library(pbmcapply)
+library(parallel)
+library(data.table)
+library(splitstackshape) # provides cSplit
+library(rvest)  # provides read_html
+library(tidyr) #provides pivot_wider
+library(XML)
+
+# get paths
+database <- databases$get("nubbe")
+
+## files
+ids <- read_delim(
+  file = database$sourceFiles$tsv,
+  delim = "\t",
+  escape_double = FALSE,
+  col_names = FALSE,
+  trim_ws = TRUE
+) %>%
+  mutate_all(as.character)
+
+X <- ids$X1
+
+list <- list()
+
+for (i in 1:length(X)) {
+  data <- xmlParse(file = X[i])
+  
+  xml_data <- xmlToList(node = data)
+  
+  id <- (xml_data$id)
+  name <- (xml_data$nome)
+  inchi <- (xml_data$inchi)
+  inchikey <- (xml_data$inchikey)
+  smiles <- (xml_data$smiles)
+  referenceYear <-
+    ifelse(
+      test = is.null(xml_data$publicacoes$artigo),
+      yes = xml_data$publicacoes$capitulo_libro$ano,
+      no = xml_data$publicacoes$artigo$ano
+    )
+  referenceAuthors <-
+    ifelse(
+      test = is.null(xml_data$publicacoes$artigo),
+      yes = xml_data$publicacoes$capitulo_libro$autores,
+      no = xml_data$publicacoes$artigo$autores
+    )
+  referenceTitle <-
+    ifelse(
+      test = is.null(xml_data$publicacoes$artigo),
+      yes = xml_data$publicacoes$capitulo_libro$titulo,
+      no = xml_data$publicacoes$artigo$titulo
+    )
+  referenceFull <-
+    ifelse(
+      test = is.null(xml_data$publicacoes$artigo),
+      yes = xml_data$publicacoes$capitulo_libro$compilado,
+      no = xml_data$publicacoes$artigo$compilado
+    )
+  organismFamily <-
+    ifelse(
+      test = xml_data$especies == "\n\t",
+      yes = NA,
+      no = (ifelse(
+        test = is.null(xml_data$especies$origem$familia),
+        yes = NA,
+        no = (xml_data$especies$origem$familia)
+      ))
+    )
+  organismGenus <-
+    ifelse(
+      test = xml_data$especies == "\n\t",
+      yes = NA,
+      no = (ifelse(
+        test = is.null(xml_data$especies$origem$genero),
+        yes = NA,
+        no = (xml_data$especies$origem$genero)
+      ))
+    )
+  organismSpecies <- ifelse(
+    test = xml_data$especies == "\n\t",
+    yes = NA,
+    no = (ifelse(
+      test = is.null(xml_data$especies$origem$especie),
+      yes = NA,
+      no = (xml_data$especies$origem$especie)
+    ))
+  )
+  
+  df <- data.frame(
+    id,
+    name,
+    inchi,
+    inchikey,
+    smiles,
+    referenceYear,
+    referenceAuthors,
+    referenceTitle,
+    referenceFull,
+    organismFamily,
+    organismGenus,
+    organismSpecies
+  )
+  
+  list[[i]] <- df
+}
+
+# manipulating
+data_original <- bind_rows(list)
+
+data_manipulated <- data_original %>%
+  mutate(
+    biologicalsource = paste(organismFamily,
+                             organismGenus,
+                             organismSpecies,
+                             sep = " "),
+    inchi = paste("InChI=", inchi, sep = "")
+  ) %>%
+  select(id,
+         name,
+         inchi,
+         inchikey,
+         smiles,
+         #temporary
+         biologicalsource,
+         reference = referenceFull)
+
+data_manipulated$biologicalsource <-
+  gsub("NA ",
+       "",
+       data_manipulated$biologicalsource,
+       fixed = TRUE)
+
+# standardizing
+data_standard <-
+  standardizing_original(
+    data_selected = data_manipulated,
+    db = "nub_1",
+    structure_field = c("inchi", "name", "smiles")
+  )
+
+# exporting
+database$writeInterim(data_standard)
