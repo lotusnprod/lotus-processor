e6cf898338c3c1d208f4a42737f20cc7cebe9231
Adafede <adriano.rutz@unige.ch>
2020-07-09 14:17:48 +0200

    refactor(paths): simplify interim paths configuration

--- paths.mk
+++ paths.mk
@@ -1,17 +1,14 @@
 export DATA_PATH ?= ${PWD}/data
-#export DATA_PATH ?= ${PWD}/data_min
 export SRC_PATH ?= ${PWD}/src
-
 
 export INTERIM_DB_PATH = ${DATA_PATH}/interim/db
 export INTERIM_DICTIONARY_PATH = ${DATA_PATH}/interim/dictionaries
 export DB_SOURCE_PATH = ${DATA_PATH}/external/dbSource
-## be carfeul with comments they can ruin the makefile as stated here: https://www.gnu.org/software/make/manual/html_node/Recipe-Syntax.html. At least it made mine do not work anymore
 
-# Below is and endless PATH nightmare. Why dont we have a unique ITERIM folder to dump all files, uniquely named ?
-## This is because the files should be slowly removed and renamed in dictionaries instead. so working with interim/original table at the begining, bunch of dictionaries (original to translated, translated to curated etc) and processed table in the end, in my view at least
-# export INTERIM_TABLE_PATH = ${DATA_PATH}/interim/tables
-export INTERIM_TABLE_PATH = ${DATA_PATH}/interim/tables_min
+# line to be (un)commented for minimal example or full version
+export INTERIM_TABLE_PATH = ${DATA_PATH}/interim/tables
+# export INTERIM_TABLE_PATH = ${DATA_PATH}/interim/tables_min
+
 export INTERIM_TABLE_ORIGINAL_PATH = ${INTERIM_TABLE_PATH}/0_original
 export INTERIM_TABLE_ORIGINAL_ORGANISM_PATH = ${INTERIM_TABLE_ORIGINAL_PATH}/organism
 export INTERIM_TABLE_ORIGINAL_REFERENCE_PATH = ${INTERIM_TABLE_ORIGINAL_PATH}/reference
