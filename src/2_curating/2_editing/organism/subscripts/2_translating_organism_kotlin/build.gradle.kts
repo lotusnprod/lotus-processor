389e71fd4e8f3a61c8dbc1e12b5b00cb6587cb73
Pierre-Marie ALLARD <allardp@FARMA-NONMEM.epgl.unige.local>
2020-07-14 09:30:42 +0200

    adding merge results

---
+++ src/2_curating/2_editing/organism/subscripts/2_translating_organism_kotlin/build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val univocityParserVersion = "2.8.4"
+group = "net.nprod.onpdb"
+version = "0.3-SNAPSHOT"
+
+plugins {
+    kotlin("jvm") version "1.4-M3"
+    application
+    id("com.github.johnrengelman.shadow") version "5.0.0"
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    // Univocity is a fast TSV reader/writer
+    implementation("com.univocity", "univocity-parsers", univocityParserVersion)
+}
+
+/**
+ * We need to compile for the outdated conda java version
+ */
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+/**
+ * We say what needs to be executed
+ */
+application {
+    mainClassName = "MainKt"
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to application.mainClassName
+            )
+        )
+    }
+}
+
+/**
+ *  We want to make a small jar, with always the same name
+ */
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    minimize()
+    archiveBaseName.set("shadow")
+    archiveClassifier.set("")
+    archiveVersion.set("")
+}
