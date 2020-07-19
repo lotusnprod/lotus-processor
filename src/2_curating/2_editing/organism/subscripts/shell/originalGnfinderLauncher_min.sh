ac5cc5996a2ea0598b9882f53fb31d4d5c657251
Adafede <adriano.rutz@unige.ch>
2020-07-19 11:17:18 +0200

    [taxonomy] starting to adapt scripts for more wikidata compatible output of taxonomies (avoid CoL) please look for CHOOSE HERE WHAT TO DO Tag

--- src/2_curating/2_editing/organism/subscripts/shell/originalGnfinderLauncher_min.sh
+++ src/2_curating/2_editing/organism/subscripts/shell/originalGnfinderLauncher_min.sh
@@ -3,7 +3,7 @@ for file in ../data/interim/tables_min/0_original/organism/*.tsv
 do
 	echo "Submitting File $file to gnfinder servers"
 	filename=$(basename $file .tsv | sed s/translated/sanitized/g) #file has extension, it return only filename, and here we add a sed line to chenge a given string in the filename
-  gnfinder find -c -l detect -s $(seq -s, 1 1 182 | sed s/,\$//) -t "0" < $file > ../data/interim/tables_min/2_cleaned/organism/original/$filename.json
+  gnfinder find -c -l detect -s 3,4,6,11,146,158,165,167,180 -t "0" < $file > ../data/interim/tables_min/2_cleaned/organism/original/$filename.json
 	echo "The result is saved as $filename.json"
 done
 
--- src/2_curating/2_editing/organism/subscripts/shell/translatedGnfinderLauncher_min.sh
+++ src/2_curating/2_editing/organism/subscripts/shell/translatedGnfinderLauncher_min.sh
@@ -4,7 +4,7 @@ do
 	echo "Submitting File $file to gnfinder servers"
 	f=$(echo "${file##*/}")
   	filename=$(echo $f| cut  -d'.' -f 1| sed  s/translated/translated/g) #file has extension, it return only filename, and here we add a sed line to chenge a given string in the filename
-	cat $file | gnfinder find -c -l detect -s $(seq -s, 1 1 182 | sed s/,\$//) -t "0" > ../data/interim/tables_min/2_cleaned/organism/translated/$filename.json
+	cat $file | gnfinder find -c -l detect -s 3,4,6,11,146,158,165,167,180 -t "0" > ../data/interim/tables_min/2_cleaned/organism/translated/$filename.json
 	echo "The result is saved as $filename.json"
 done
 
