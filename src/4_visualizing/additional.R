b4613e23558aabdf512c07412b4e15cf8140e60a
Adafede <adriano.rutz@unige.ch>
2020-09-25 10:42:01 +0200

    feat(ui): add automatic removal of manually rejected entries from validated set

---
+++ src/4_visualizing/additional.R
@@ -0,0 +1,32 @@
+# additional fig
+
+# needs 3_metrics
+fig1 <- plot_ly(type = 'violin')  %>%
+  add_trace(
+    x = 1,
+    y = organismsPerStructure$n,
+    legendgroup = 'organisms per structure',
+    scalegroup = 'organisms per structure',
+    name = 'organisms per structure',
+    side = 'negative',
+    box = list(visible = T),
+    meanline = list(visible = T),
+    color = I("#a6cee3")
+  ) %>%
+  add_trace(
+    x = 1,
+    y = structuresPerOrganism$n,
+    legendgroup = 'structures per organism',
+    scalegroup = 'structures per organism',
+    name = 'structures per organism',
+    side = 'positive',
+    box = list(visible = T),
+    meanline = list(visible = T),
+    color = I("#1f78b4")
+  ) %>%
+  layout(yaxis = list(
+    range = c(0, 500),
+    title = "",
+    zeroline = F
+  ))
+fig1
