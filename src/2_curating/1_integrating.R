fe9177e733959fffa802076db4d12de2b968aef7
Adafede <adriano.rutz@unige.ch>
2021-01-05 09:23:59 +0100

    [triples] adding first lines

--- src/2_curating/1_integrating.R
+++ src/2_curating/1_integrating.R
@@ -75,6 +75,20 @@ if (mode != "test") {
     ) %>%
     tibble()
 
+  ## for triples
+  # triples <- dbTable %>%
+  #   rowid_to_column("subject") %>%
+  #   mutate(subject = paste0("LTS", str_pad(
+  #     string = subject,
+  #     width = 7,
+  #     pad = "0"
+  #   ))) %>%
+  #   gather(predicate,
+  #          object,
+  #          -subject,
+  #          na.rm = TRUE) %>%
+  #   arrange(subject)
+
   if (mode == "min") {
     cat("sampling rows for min mode \n")
   }
