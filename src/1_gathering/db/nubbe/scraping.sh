988889df4927c46b3229e7accf9102cb305f5c1c
Jonathan Bisson <bjonnh-code@bjonnh.net>
2021-01-29 18:35:11 -0600

    [Dev] Trying but not able to test a corrected Nubbe download script.

--- src/1_gathering/db/nubbe/scraping.sh
+++ src/1_gathering/db/nubbe/scraping.sh
@@ -1,9 +1,15 @@
-curl 'https://nubbe.iq.unesp.br/portal/do/Query' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'X-Requested-With: XMLHttpRequest' -H 'Origin: https://nubbe.iq.unesp.br' -H 'Referer: https://nubbe.iq.unesp.br/portal/nubbe-search.html' --data-raw 'service=17&tipo_1=' > ../data/external/dbSource/nubbe/nubbe_ids.xml
-echo "" > ../data/external/dbSource/nubbe/folder/download.txt
-for i in $(grep -o '<id>.*</id>' ../data/external/dbSource/nubbe/nubbe_ids.xml | sed 's/\(<id>\|<\/id>\)//g' | sort -u); do
+#!/usr/bin/env bash
+export DB_DIR=./data/external/dbSource/nubbe
+export DB_DIR_OUT="$DB_DIR"/folder
+mkdir -p "$DB_DIR" "$DB_DIR_OUT"
 
-    echo "url=\"https://nubbe.iq.unesp.br/portal/do/Query?service=21&id=$i\"" >> ../data/external/dbSource/nubbe/folder/download.txt
-    echo "output=\"$i.xml\"" >> ../data/external/dbSource/nubbe/folder/download.txt
+curl 'https://nubbe.iq.unesp.br/portal/do/Query' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'X-Requested-With: XMLHttpRequest' -H 'Origin: https://nubbe.iq.unesp.br' -H 'Referer: https://nubbe.iq.unesp.br/portal/nubbe-search.html' --data-raw 'service=17&tipo_1=' > "$DB_DIR"/nubbe_ids.xml
+rm "$DB_DIR_OUT"/download.txt
+for i in $(grep -o '<id>.*</id>' "$DB_DIR"/nubbe_ids.xml | sed 's/\(<id>\|<\/id>\)//g' | sort -u); do
+    echo "url=\"https://nubbe.iq.unesp.br/portal/do/Query?service=21&id=$i\"" >> "$DB_DIR_OUT"/download.txt
+    echo "output=\"$i.xml\"" >> "$DB_DIR_OUT"/download.txt
 done
-curl -K ../data/external/dbSource/nubbe/folder/download.txt
-rm ../data/external/dbSource/nubbe/folder/download.txt
+
+cd "$DB_DIR_OUT"
+curl -K "$DB_DIR_OUT"/download.txt
+rm "$DB_DIR_OUT"/download.txt
