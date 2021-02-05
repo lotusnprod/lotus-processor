1da23f5443c168447f2d72698e760b5806f50fa8
Adafede <adriano.rutz@unige.ch>
2021-02-05 11:08:41 +0100

    adding new DB schema

--- src/schema_db/0000_create_initial_tables.sql
+++ src/schema_db/0000_create_initial_tables.sql
@@ -1,288 +1,313 @@
-create table chemical_databases
+create table curation_type
 (
-	id INTEGER not null
-		constraint chemical_databases_pk
-			primary key autoincrement,
-	name TEXT
+    id   INTEGER not null
+        constraint curation_states_pk
+            primary key autoincrement,
+    name TEXT
 );
 
-create unique index chemical_databases_id_uindex
-	on chemical_databases (id);
+create
+    unique index curation_type_id_uindex
+    on curation_type (id);
 
-create table chemical_information
+create
+    unique index curation_type_name_uindex
+    on curation_type (name);
+
+create table data_cleaned
 (
-	id INTEGER not null
-		constraint chemical_information_pk
-			primary key autoincrement,
-	cleanedStructureId INTEGER
-		references structure_cleaned,
-	chemicalDatabaseId INTEGER
-		references chemical_databases,
-	chemicalId TEXT,
-	ranks TEXT,
-	taxonomy TEXT,
-	rank TEXT
+    id                 INTEGER not null
+        constraint data_cleaned_pk
+            primary key autoincrement,
+    structureCleanedId INTEGER
+        references structure_cleaned,
+    organismCleanedId  INTEGER
+        references organism_cleaned,
+    referenceCleanedId INTEGER
+        references reference_cleaned,
+    curationTypeId     INTEGER
+        references curation_type
 );
 
-create unique index chemical_information_id_uindex
-	on chemical_information (id);
-	
-create table curation_states
-(
-	id INTEGER not null
-		constraint curation_states_pk
-			primary key autoincrement,
-	name TEXT
-);
-
-create unique index curation_states_id_uindex
-	on curation_states (id);
-
-create unique index curation_states_name_uindex
-	on curation_states (name);
-
-create table data_processed
-(
-	id INTEGER not null
-		constraint processed_data_pk
-			primary key autoincrement,
-	structureCleanedId INTEGER
-		references structure_cleaned,
-	organismCleanedId INTEGER
-		references organism_cleaned,
-	referenceCleanedId INTEGER
-		references reference_cleaned
-);
-
-create unique index data_processed_id_uindex
-	on data_processed (id);
+create
+    unique index data_cleaned_id_uindex
+    on data_cleaned (id);
 
 create table data_source
 (
-	id INTEGER not null
-		constraint source_data_pk
-			primary key autoincrement,
-	databaseSourceId INTEGER
-		references database_source,
-	organismSourceId INTEGER
-		references organism_source,
-	structureSourceId INTEGER
-		references structure_source,
-	referenceSourceId INTEGER
-		references reference_source
+    id                INTEGER not null
+        constraint data_source_pk
+            primary key autoincrement,
+    databaseSourceId  INTEGER
+        references database_source,
+    organismSourceId  INTEGER
+        references organism_source,
+    structureSourceId INTEGER
+        references structure_source,
+    referenceSourceId INTEGER
+        references reference_source
 );
 
-create unique index data_source_id_uindex
-	on data_source (id);
+create
+    unique index data_source_id_uindex
+    on data_source (id);
 
-create table data_processed__data_source
+create table data_source__data_cleaned
 (
-	id INTEGER not null
-		constraint processed_data__source_data_pk
-			primary key autoincrement,
-	dataSourceId INTEGER
-		references data_source,
-	dataProcessedId INTEGER
-		references data_processed,
-	curationStateId INTEGER
-		references curation_states
+    id            INTEGER not null
+        constraint data_source__data_cleaned_pk
+            primary key autoincrement,
+    dataSourceId  INTEGER
+        references data_source,
+    dataCleanedId INTEGER
+        references data_cleaned
 );
 
-create unique index data_processed__data_source_id_uindex
-	on data_processed__data_source (id);
-	
-create table database_types
+create
+    unique index data_source__data_cleaned_id_uindex
+    on data_source__data_cleaned (id);
+
+create table database_type
 (
-	id INTEGER not null
-		constraint database_types_pk
-			primary key autoincrement,
-	name TEXT
+    id   INTEGER not null
+        constraint database_type_pk
+            primary key autoincrement,
+    name TEXT
 );
 
 create table database_source
 (
-	id INTEGER not null
-		constraint source_database_pk
-			primary key autoincrement,
-	name TEXT,
-	typeId INTEGER
-		references database_types
+    id             INTEGER not null
+        constraint database_source_pk
+            primary key autoincrement,
+    name           TEXT,
+    databaseTypeId INTEGER
+        references database_type
 );
-	
-create unique index database_source_id_uindex
-	on database_source (id);
 
-create unique index database_source_name_uindex
-	on database_source (name);
+create
+    unique index database_source_id_uindex
+    on database_source (id);
+
+create
+    unique index database_source_name_uindex
+    on database_source (name);
 
 create table organism_cleaned
 (
-	id INTEGER not null
-		constraint organism_cleaned_pk
-			primary key autoincrement,
-	name TEXT
+    id   INTEGER not null
+        constraint organism_cleaned_pk
+            primary key autoincrement,
+    name TEXT
 );
 
-create unique index organism_cleaned_id_uindex
-	on organism_cleaned (id);
+create
+    unique index organism_cleaned_id_uindex
+    on organism_cleaned (id);
 
-create table organism_synonyms
+create table organism_synonym
 (
-	id INTEGER not null
-		constraint organism_synonyms_pk
-			primary key autoincrement,
-	name TEXT,
-	organismCleanedId int
-		references organism_cleaned
+    id                INTEGER not null
+        constraint organism_synonym_pk
+            primary key autoincrement,
+    name              TEXT,
+    organismCleanedId int
+        references organism_cleaned
 );
 
-create unique index organism_synonyms_id_uindex
-	on organism_synonyms (id);
-
-create table organism_types
+create table organism_database
 (
-	id INTEGER not null
-		constraint organism_types_pk
-			primary key autoincrement,
-	name TEXT
+    id   INTEGER not null
+        constraint organism_database_pk
+            primary key autoincrement,
+    name TEXT
+);
+
+create
+    unique index organism_database_id_uindex
+    on organism_database (id);
+
+create table organism_information
+(
+    id                 INTEGER not null
+        constraint organism_information_pk
+            primary key autoincrement,
+    organismCleanedId  INTEGER
+        references organism_cleaned,
+    organismDatabaseId INTEGER
+        references organism_database,
+    taxonId            TEXT,
+    ranks              TEXT,
+    taxonomy           TEXT,
+    rank               TEXT
+);
+
+create
+    unique index organism_information_id_uindex
+    on organism_information (id);
+
+create
+    unique index organism_synonym_id_uindex
+    on organism_synonym (id);
+
+create table organism_type
+(
+    id   INTEGER not null
+        constraint organism_type_pk
+            primary key autoincrement,
+    name TEXT
 );
 
 create table organism_source
 (
-	id INTEGER not null
-		constraint organism_source_pk
-			primary key autoincrement,
-	value TEXT,
-	typeId INTEGER
-		references organism_types
+    id             INTEGER not null
+        constraint organism_source_pk
+            primary key autoincrement,
+    value          TEXT,
+    organismTypeId INTEGER
+        references organism_type
 );
 
 create table reference_cleaned
 (
-	id INTEGER not null
-		constraint reference_cleaned_pk
-			primary key autoincrement,
-	doi TEXT,
-	pmcid TEXT,
-	pmid TEXT,
-	title TEXT
+    id    INTEGER not null
+        constraint reference_cleaned_pk
+            primary key autoincrement,
+    doi   TEXT,
+    pmcid TEXT,
+    pmid  TEXT,
+    title TEXT
 );
 
-create unique index reference_cleaned_id_uindex
-	on reference_cleaned (id);
+create
+    unique index reference_cleaned_id_uindex
+    on reference_cleaned (id);
 
-create table reference_types
+
+create table reference_database
 (
-	id INTEGER not null
-		constraint reference_types_pk
-			primary key autoincrement,
-	name TEXT
+    id   INTEGER not null
+        constraint reference_database_pk
+            primary key autoincrement,
+    name TEXT
+);
+
+create
+    unique index reference_database_id_uindex
+    on reference_database (id);
+
+create table reference_information
+(
+    id                  INTEGER not null
+        constraint reference_information_pk
+            primary key autoincrement,
+    referenceCleanedId  INTEGER
+        references reference_cleaned,
+    referenceDatabaseId INTEGER
+        references reference_database,
+    referenceId         TEXT,
+    data                TEXT
+);
+
+create
+    unique index reference_information_id_uindex
+    on reference_information (id);
+
+create table reference_type
+(
+    id   INTEGER not null
+        constraint reference_type_pk
+            primary key autoincrement,
+    name TEXT
 );
 
 create table reference_source
 (
-	id INTEGER not null
-		constraint reference_source_pk
-			primary key autoincrement,
-	value TEXT,
-	typeId INTEGER
-		references reference_types
+    id              INTEGER not null
+        constraint reference_source_pk
+            primary key autoincrement,
+    value           TEXT,
+    referenceTypeId INTEGER
+        references reference_type
 );
 
 create table structure_cleaned
 (
-	id INTEGER not null
-		constraint cleaned_compounds_pk
-			primary key autoincrement,
-	traditionalName TEXT,
-	iupacName TEXT,
-	inchikey TEXT,
-	inchikey2D TEXT,
-	inchi TEXT,
-	inchi2D TEXT,
-	smiles TEXT,
-	smiles2D TEXT,
-	stereocentersTotal INTEGER,
-	stereocentersUnspecified INTEGER,
-	molecularFormula TEXT,
-	exactMass REAL,
-	xlogp REAL
+    id                       INTEGER not null
+        constraint structure_cleaned_pk
+            primary key autoincrement,
+    traditionalName          TEXT,
+    iupacName                TEXT,
+    inchikey                 TEXT,
+    inchikey2D               TEXT,
+    inchi                    TEXT,
+    inchi2D                  TEXT,
+    smiles                   TEXT,
+    smiles2D                 TEXT,
+    stereocentersTotal       INTEGER,
+    stereocentersUnspecified INTEGER,
+    molecularFormula         TEXT,
+    exactMass                REAL,
+    xlogp                    REAL
 );
 
+create
+    unique index structure_cleaned_id_uindex
+    on structure_cleaned (id);
 
-create unique index structure_cleaned_id_uindex
-	on structure_cleaned (id);
-
-create table structure_types
+create table structure_cleaned__structure_information
 (
-	id INTEGER not null
-		constraint structure_types_pk
-			primary key autoincrement,
-	name TEXT
+    id                     INTEGER not null
+        constraint structure_cleaned__structure_information_pk
+            primary key autoincrement,
+    structureCleanedId     INTEGER
+        references structure_cleaned,
+    structureInformationId INTEGER
+        references structure_information
+);
+
+create
+    unique index structure_cleaned__structure_information_id_uindex
+    on structure_cleaned__structure_information (id);
+
+create table structure_information
+(
+    id                         INTEGER not null
+        constraint structure_information_pk
+            primary key autoincrement,
+    structureInformationTypeId INTEGER
+        references structure_information_type,
+    valueString                TEXT,
+    valueInt                   INTEGER,
+    valueReal                  REAL
+);
+
+create
+    unique index structure_information_id_uindex
+    on structure_information (id);
+
+create table structure_information_type
+(
+    id   INTEGER not null
+        constraint structure_information_type_pk
+            primary key autoincrement,
+    name TEXT
 );
 
 create table structure_source
 (
-	id INTEGER not null
-		constraint structure_source_pk
-			primary key autoincrement,
-	value TEXT,
-	typeId INTEGER
-		references structure_types
+    id              INTEGER not null
+        constraint structure_source_pk
+            primary key autoincrement,
+    value           TEXT,
+    structureTypeId INTEGER
+        references structure_type
 );
 
-create table reference_databases
+create table structure_type
 (
-	id INTEGER not null
-		constraint reference_databases_pk
-			primary key autoincrement,
-	name TEXT
+    id   INTEGER not null
+        constraint structure_type_pk
+            primary key autoincrement,
+    name TEXT
 );
-
-create unique index reference_databases_id_uindex
-	on reference_databases (id);
-
-create table reference_information
-(
-	id INTEGER not null
-		constraint reference_information_pk
-			primary key autoincrement,
-	cleanedReferenceId INTEGER
-		references reference_cleaned,
-	referenceDatabaseId INTEGER
-		references reference_databases,
-	referenceId TEXT,
-	data TEXT
-);
-
-create unique index reference_information_id_uindex
-	on reference_information (id);
-	
-create table taxonomic_databases
-(
-	id INTEGER not null
-		constraint taxonomic_databases_pk
-			primary key autoincrement,
-	name TEXT
-);
-
-create unique index taxonomic_databases_id_uindex
-	on taxonomic_databases (id);
-
-create table taxonomic_information
-(
-	id INTEGER not null
-		constraint taxonomic_information_pk
-			primary key autoincrement,
-	cleanedOrganismId INTEGER
-		references organism_cleaned,
-	taxonomicDatabaseId INTEGER
-		references taxonomic_databases,
-	taxonomicId TEXT,
-	ranks TEXT,
-	taxonomy TEXT,
-	rank TEXT
-);
-
-create unique index taxonomic_information_id_uindex
-	on taxonomic_information (id);
