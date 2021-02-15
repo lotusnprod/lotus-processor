0c7d7c802cfe2620d5020ba08cfffe36953490a1
Adafede <adriano.rutz@unige.ch>
2021-02-15 09:03:49 +0100

    style(schema_db): improve table creation and join syntax consistency

--- src/schema_db/0000_create_initial_tables.sql
+++ src/schema_db/0000_create_initial_tables.sql
@@ -171,7 +171,7 @@ create table organism_source
 create table ott_taxonomy
 (
     uid        INTEGER not null,
-    parent_uid  INTEGER,
+    parent_uid INTEGER,
     name       TEXT,
     rank       TEXT,
     sourceinfo TEXT,
