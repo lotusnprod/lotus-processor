e172af225f8dc0652e3e4f9dfdf027b470484abe
Adafede <adriano.rutz@unige.ch>
2021-02-24 17:20:42 +0100

    feat(env): set bioconductor as repo for some packages

--- environment_non_conda.sh
+++ environment_non_conda.sh
@@ -5,3 +5,11 @@ echo "Installing packages missing from conda"
 R --slave -e "install.packages('rcrossref', repo=\"https://cran.rstudio.com/\")"
 
 R --slave -e "install.packages('classyfireR', repo=\"https://cran.rstudio.com/\")"
+
+R --slave -e "install.packages('ggstar', repo=\"https://cran.rstudio.com/\")"
+
+R --slave -e "install.packages('ggtree', repo=\"https://bioconductor.org/\")"
+
+R --slave -e "install.packages('ggtreeExtra', repo=\"https://bioconductor.org/\")"
+
+R --slave -e "install.packages('treeio', repo=\"https://bioconductor.org/\")"
