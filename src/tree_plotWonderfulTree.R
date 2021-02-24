c4f279cdadc27dd31132fa61578c1dfead66b218
Adafede <adriano.rutz@unige.ch>
2021-02-24 08:21:37 +0100

    adding plotting tree scripts adapted

--- src/tree_plotWonderfulTree.R
+++ src/tree_plotWonderfulTree.R
@@ -1,33 +1,103 @@
 cat("This script plots the wonderful tree. \n")
-cat("It currently needs 'tree_prepareTree.R' to be run before. \n")
 
 start <- Sys.time()
 
+library(data.table)
+library(ggtree)
 library(ggtreeExtra)
 library(ggstar)
 library(ggnewscale)
+library(rotl)
+library(splitstackshape)
+library(tidyverse)
+library(treeio)
+source(file = "paths.R")
 source(file = "r/colors.R")
 
+pairs_metadata <- fread(
+  input = file.path(
+    pathDataProcessed,
+    "210223_frozen_metadata.csv.gz"
+  ),
+  na.strings = ""
+)
+
+n_min <- 50
+
+families_restricted <- pairs_metadata %>%
+  filter(!is.na(organism_taxonomy_06family)) %>%
+  group_by(organism_taxonomy_06family) %>%
+  add_count() %>%
+  ungroup() %>%
+  filter(n >= n_min) %>%
+  distinct(organism_taxonomy_06family)
+
+families_matched_restricted <-
+  tnrs_match_names(
+    names = families_restricted$organism_taxonomy_06family,
+    do_approximate_matching = FALSE
+  )
+
+ott_in_tree <-
+  ott_id(families_matched_restricted)[is_in_tree(ott_id(families_matched_restricted))]
+
+families_restricted <- families_restricted %>%
+  filter(organism_taxonomy_06family %in% names(ott_in_tree))
+
+families_matched_restricted <-
+  tnrs_match_names(names = families_restricted$organism_taxonomy_06family)
+
+families_matched_restricted <- families_matched_restricted %>%
+  mutate(key = paste(
+    gsub(
+      x = unique_name,
+      pattern = " ",
+      replacement = "_",
+      fixed = TRUE
+    ),
+    paste0("ott", ott_id),
+    sep = "_"
+  ))
+
+tr_restricted <- tol_induced_subtree(ott_ids = ott_in_tree)
+
 test <- pairs_metadata %>%
-  filter(Family %in% families_restricted$Family) %>%
+  cSplit(
+    splitCols = colnames(.)[.[, grepl(
+      pattern = "structure_taxonomy_npclassifier_",
+      x = colnames(.)
+    )]],
+    sep = "|",
+    direction = "long"
+  ) %>%
+  filter(
+    !is.na(structure_taxonomy_npclassifier_01pathway) &
+      !is.na(structure_taxonomy_npclassifier_02superclass) &
+      !is.na(structure_taxonomy_npclassifier_03class)
+  ) %>%
+  mutate_all(as.character) %>%
+  filter(organism_taxonomy_06family %in% families_matched_restricted$unique_name) %>%
   # filter(!is.na(`03class`)) %>%
-  filter(!is.na(class)) %>%
-  distinct(Family,
-    structureCleaned_inchikey2D,
+  filter(!is.na(structure_taxonomy_npclassifier_03class)) %>%
+  distinct(organism_taxonomy_06family,
+    structure_inchikey,
     .keep_all = TRUE
   ) %>%
-  group_by(Family) %>%
+  group_by(organism_taxonomy_06family) %>%
   add_count(name = "n") %>%
   # group_by(`03class`) %>%
-  group_by(class) %>%
+  group_by(structure_taxonomy_npclassifier_03class) %>%
   add_count(name = "m") %>%
   group_by(
-    Family,
-    Genus
+    organism_taxonomy_06family,
+    organism_taxonomy_08genus
   ) %>%
   add_count(name = "o") %>%
   # group_by(Family, `03class`) %>%
-  group_by(Family, class) %>%
+  group_by(
+    organism_taxonomy_06family,
+    structure_taxonomy_npclassifier_03class
+  ) %>%
   add_count(name = "p") %>%
   mutate(q = p^2 / (m * n)) %>%
   # distinct(Family,
@@ -36,16 +106,17 @@ test <- pairs_metadata %>%
   #   o,
   #   .keep_all = TRUE
   # ) %>%
-  distinct(Family,
-    class,
+  distinct(
+    organism_taxonomy_06family,
+    structure_taxonomy_npclassifier_03class,
     q,
     o,
     .keep_all = TRUE
   ) %>%
-  group_by(Family) %>%
+  group_by(organism_taxonomy_06family) %>%
   filter(q == max(q)) %>%
   ungroup() %>%
-  distinct(Family, q, .keep_all = TRUE)
+  distinct(organism_taxonomy_06family, q, .keep_all = TRUE)
 
 tr_restricted$tip.label <-
   gsub(
@@ -57,16 +128,15 @@ tr_restricted$tip.label <-
 taxonomy <-
   left_join(
     families_restricted,
-    pairs_metadata %>%
-      distinct(
-        Domain,
-        Kingdom,
-        Phylum,
-        Class,
-        Order,
-        Infraorder,
-        Family
-      )
+    pairs_metadata
+  ) %>%
+  distinct(
+    Domain = organism_taxonomy_01domain,
+    Kingdom = organism_taxonomy_02kingdom,
+    Phylum = organism_taxonomy_03phylum,
+    Class = organism_taxonomy_04class,
+    Order = organism_taxonomy_05order,
+    Family = organism_taxonomy_06family
   )
 
 info <- taxonomy %>%
@@ -79,18 +149,25 @@ info <- taxonomy %>%
 
 bar_data <- test %>%
   select(
-    id = Family,
+    id = organism_taxonomy_06family,
     everything(),
-    -Domain,
-    -Kingdom,
-    -Phylum,
-    -Class,
-    -Order
+    -organism_taxonomy_01domain,
+    -organism_taxonomy_02kingdom,
+    -organism_taxonomy_03phylum,
+    -organism_taxonomy_05order
   ) %>%
   # mutate(`02superclass` = fct_reorder(`02superclass`, `01kingdom`)) %>%
   # mutate(`03class` = fct_reorder(`03class`, !is.na(`02superclass`)))
-  mutate(superclass = fct_reorder(superclass, pathway)) %>%
-  mutate(class = fct_reorder(class, !is.na(superclass)))
+  mutate(
+    superclass = fct_reorder(
+      structure_taxonomy_npclassifier_02superclass,
+      structure_taxonomy_npclassifier_01pathway
+    )
+  ) %>%
+  mutate(class = fct_reorder(
+    structure_taxonomy_npclassifier_03class,
+    !is.na(structure_taxonomy_npclassifier_02superclass)
+  ))
 
 bar_data_temp <- bar_data
 
@@ -98,7 +175,7 @@ info_temp <- info %>%
   filter(id %in% bar_data_temp$id)
 
 ott_temp <-
-  ott_in_tree_restricted[names(ott_in_tree_restricted) %in% bar_data_temp$id]
+  ott_in_tree[names(ott_in_tree) %in% bar_data_temp$id]
 
 ott_temp <- ott_temp[!duplicated(ott_temp)]
 
@@ -134,7 +211,7 @@ p <- p %<+%
         y = id,
         x = q,
         # fill = `02superclass`
-        fill = pathway
+        fill = structure_taxonomy_npclassifier_01pathway
       ),
     offset = rel(0.2),
     pwidth = rel(1.6),
@@ -144,8 +221,12 @@ p <- p %<+%
   geom_tiplab(
     mapping = aes(
       # label = paste(`02superclass`, `03class`, sep = " - "),
-      label = paste(superclass, class, sep = " - "),
-      x = 26 + 63 * q ## to adapt with the data
+      label = paste(
+        structure_taxonomy_npclassifier_02superclass,
+        structure_taxonomy_npclassifier_03class,
+        sep = " - "
+      ),
+      x = 33 + 75 * q ## to adapt with the data
     ),
     linetype = "blank",
     linesize = 0,
