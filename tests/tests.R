d950ea90d36c3eab10c102fbb646ae3eb6a8d62b
Adafede <adriano.rutz@unige.ch>
2021-02-27 16:25:17 +0100

    docs(environment.yml): update dependencies to include r-testthat package

--- tests/tests.R
+++ tests/tests.R
@@ -7,39 +7,6 @@ library(tidyverse)
 source("paths.R")
 source("r/log.R")
 
-#' Check if taxon is attributed to the right kingdom
-#'
-#' @param table_organisms The table contaning the taxon/taxa you want to test
-#' @param taxon_detected The taxon/taxa you want to test
-#' @param kingdom_cleaned The kingdom(s) you want them to belong to
-#'
-#' @return If expectation is met or not
-#' @export
-#'
-#' @examples
-#' expect_taxon(organismTableFull, c("Iris"), c("Plantae", "Viridiplantae", NA))
-expect_taxon <-
-  function(table_organisms,
-           taxon_detected,
-           kingdom_cleaned) {
-    act <- quasi_label(enquo(taxon_detected), arg = "object")
-    act$kingdom <- table_organisms %>%
-      filter(word(string = taxon_detected, end = 1) == organismDetected) %>%
-      distinct(organismDetected, organismCleaned_dbTaxo_1kingdom)
-    expect(
-      ok = all(
-        sort(act$kingdom$organismCleaned_dbTaxo_1kingdom) %in% sort(kingdom_cleaned)
-      ),
-      failure_message = sprintf(
-        "%s belongs to %s, not  %s.",
-        act$lab,
-        sort(act$kingdom$organismCleaned_dbTaxo_1kingdom),
-        sort(kingdom_cleaned)
-      )
-    )
-    invisible(act$val)
-  }
-
 log_debug("loading ...")
 log_debug("... initial table")
 originalTable <- read_delim(
@@ -60,24 +27,6 @@ organismTableFull <- read_delim(
   escape_double = FALSE,
   trim_ws = TRUE
 ) %>%
-  select(
-    organismType,
-    organismValue,
-    organismDetected,
-    organismCleaned,
-    organismCleaned_dbTaxo = organismDbTaxo,
-    # organismCleaned_dbTaxoTaxonIds = organismTaxonIds,
-    organismCleaned_dbTaxoTaxonRanks = organismTaxonRanks,
-    organismCleaned_dbTaxoTaxonomy = organismTaxonomy,
-    organismCleaned_dbTaxo_1kingdom = organism_1_kingdom,
-    organismCleaned_dbTaxo_2phylum = organism_2_phylum,
-    organismCleaned_dbTaxo_3class = organism_3_class,
-    organismCleaned_dbTaxo_4order = organism_4_order,
-    organismCleaned_dbTaxo_5family = organism_5_family,
-    organismCleaned_dbTaxo_6genus = organism_6_genus,
-    organismCleaned_dbTaxo_7species = organism_7_species,
-    organismCleaned_dbTaxo_8variety = organism_8_variety
-  ) %>%
   tibble()
 
 log_debug("... structures ...")
@@ -99,22 +48,6 @@ cleanedStructureTableFull <- read_delim(
   escape_double = FALSE,
   trim_ws = TRUE
 ) %>%
-  select(
-    structureTranslated,
-    structureCleanedSmiles = smilesSanitized,
-    structureCleanedInchi = inchiSanitized,
-    structureCleanedInchikey = inchikeySanitized,
-    structureCleaned_inchikey2D = shortikSanitized,
-    structureCleaned_inchi2D = inchiSanitizedFlat,
-    structureCleaned_smiles2D = smilesSanitizedFlat,
-    structureCleaned_molecularFormula = formulaSanitized,
-    structureCleaned_exactMass = exactmassSanitized,
-    structureCleaned_xlogp = xlogpSanitized,
-    structureCleaned_stereocenters_unspecified = count_unspecified_atomic_stereocenters,
-    structureCleaned_stereocenters_total = count_atomic_stereocenters,
-    structureCleaned_nameIupac,
-    structureCleaned_nameTraditional
-  ) %>%
   tibble()
 
 ## no classification (classyFire, NP-classifier) test at the moment
@@ -127,7 +60,6 @@ referenceTableFull <- read_delim(
   escape_double = FALSE,
   trim_ws = TRUE
 ) %>%
-  mutate(referenceCleanedDoi = toupper(referenceCleanedDoi)) %>%
   select(-referenceCleaned_score_crossref) %>%
   tibble()
 
@@ -156,7 +88,6 @@ structureFullExpectation <- read_delim(
   escape_double = FALSE,
   trim_ws = TRUE
 ) %>%
-  select(-structureCleaned_validatorLog) %>%
   tibble()
 
 log_debug("... references")
@@ -167,35 +98,28 @@ referenceTableFullExpectation <- read_delim(
   escape_double = FALSE,
   trim_ws = TRUE
 ) %>%
-  select(-referenceCleaned_score_crossref) %>%
   tibble()
 
 log_debug("testing ...")
 log_debug("... organisms")
 test_that(
   desc = "organisms",
-  code = expect_equal(
-    object = organismTableFull,
-    expected = organismTableFullExpectation
-  )
+  code = expect_equal(object = organismTableFull,
+                      expected = organismTableFullExpectation)
 )
 
 log_debug("... structures")
 test_that(
   desc = "structures",
-  code = expect_equal(
-    object = structureFull,
-    expected = structureFullExpectation
-  )
+  code = expect_equal(object = structureFull,
+                      expected = structureFullExpectation)
 )
 
 log_debug("... references")
 test_that(
   desc = "references",
-  code = expect_equal(
-    object = referenceTableFull,
-    expected = referenceTableFullExpectation
-  )
+  code = expect_equal(object = referenceTableFull,
+                      expected = referenceTableFullExpectation)
 )
 
 # write.table(
@@ -206,7 +130,7 @@ test_that(
 #   sep = "\t",
 #   fileEncoding = "UTF-8"
 # )
-#
+
 # write.table(
 #   x = structureFull,
 #   file = pathTestsStructures,
@@ -215,13 +139,12 @@ test_that(
 #   sep = "\t",
 #   fileEncoding = "UTF-8"
 # )
-#
+
 # write.table(
 #   x = referenceTableFull,
-#   file = pathTestsReferencces,
+#   file = pathTestsReferences,
 #   row.names = FALSE,
 #   quote = FALSE,
 #   sep = "\t",
 #   fileEncoding = "UTF-8"
 # )
-
