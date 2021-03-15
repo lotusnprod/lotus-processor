f93d03d74725c248d3b1938206dc38083f42d4ce
Adafede <adriano.rutz@unige.ch>
2021-03-15 16:34:13 +0100

    feat(gathering/translation): update GBIF backbone taxonomy files

---
+++ src/1_gathering/translation/get_gbif_backbone.sh
@@ -0,0 +1,3 @@
+# wget "https://hosted-datasets.gbif.org/datasets/backbone/backbone-current.zip" -O ../data/external/translationSource/common/backbone-current.zip
+
+wget "https://hosted-datasets.gbif.org/datasets/backbone/2021-03-03/backbone.zip" -O ../data/external/translationSource/common/backbone-current.zip
