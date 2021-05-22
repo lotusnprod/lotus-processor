f53b15dd82cebf86bf3383eafde3016d0b41c9f8
Adafede <adriano.rutz@unige.ch>
2021-05-22 18:50:21 +0200

    style: refactor npclassifier functions to improve readability and consistency

--- src/r/treat_npclassifier_json.R
+++ src/r/treat_npclassifier_json.R
@@ -11,33 +11,39 @@ treat_npclassifier_json <- function() {
   taxonomy_classes <- taxonomy_classes %>%
     t() %>%
     data.frame() %>%
-    mutate(class = rownames(.),
-           id_class = as.numeric(id_class))
-  
+    mutate(
+      class = rownames(.),
+      id_class = as.numeric(id_class)
+    )
+
   taxonomy_superclasses <- taxonomy$Superclass %>%
     rbind()
   rownames(taxonomy_superclasses) <- "id_superclass"
   taxonomy_superclasses <- taxonomy_superclasses %>%
     t() %>%
     data.frame() %>%
-    mutate(superclass = rownames(.),
-           id_superclass = as.numeric(id_superclass))
-  
+    mutate(
+      superclass = rownames(.),
+      id_superclass = as.numeric(id_superclass)
+    )
+
   taxonomy_pathways <- taxonomy$Pathway %>%
     rbind()
   rownames(taxonomy_pathways) <- "id_pathway"
   taxonomy_pathways <- taxonomy_pathways %>%
     t() %>%
     data.frame() %>%
-    mutate(pathway = rownames(.),
-           id_pathway = as.numeric(id_pathway))
-  
+    mutate(
+      pathway = rownames(.),
+      id_pathway = as.numeric(id_pathway)
+    )
+
   taxonomy_hierarchy_class <- taxonomy$Class_hierarchy
-  
+
   id_pathway <- list()
   id_superclass <- list()
   id_class <- list()
-  
+
   for (i in seq_along(1:length(taxonomy_hierarchy_class))) {
     id_pathway[[i]] <- taxonomy_hierarchy_class[[i]]$Pathway
     id_superclass[[i]] <- taxonomy_hierarchy_class[[i]]$Superclass
@@ -48,13 +54,13 @@ treat_npclassifier_json <- function() {
     mutate(id_class = as.numeric(id_class)) %>%
     unnest(id_superclass) %>%
     unnest(id_pathway)
-  
+
   ## No idea why would this be needed... class already has everything?
   id_pathway_2 <- list()
   id_superclass <- list()
-  
+
   taxonomy_hierarchy_superclass <- taxonomy$Super_hierarchy
-  
+
   for (i in seq_along(1:length(taxonomy_hierarchy_superclass))) {
     id_pathway_2[[i]] <- taxonomy_hierarchy_superclass[[i]]$Pathway
     id_superclass[[i]] <- names(taxonomy_hierarchy_superclass[i])
@@ -63,11 +69,11 @@ treat_npclassifier_json <- function() {
     data.frame() %>%
     mutate(id_superclass = as.numeric(id_superclass)) %>%
     unnest(id_pathway_2)
-  
+
   taxonomy_semicleaned <- full_join(zu, taxonomy_classes) %>%
     full_join(., taxonomy_superclasses) %>%
     full_join(., taxonomy_pathways) %>%
     distinct(class, superclass, pathway)
-  
+
   return(taxonomy_semicleaned)
 }
