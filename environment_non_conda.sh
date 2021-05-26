be3cde5114ba60e48e698e57fdce85600fd5d4e2
Adafede <adriano.rutz@unige.ch>
2021-05-26 09:28:52 +0200

    update

--- environment_non_conda.sh
+++ environment_non_conda.sh
@@ -2,12 +2,12 @@
 
 echo "Installing packages missing from conda"
 
-R --slave -e "install.packages('rcrossref', repo=\"https://cran.rstudio.com/\")"
+R --slave -e "install.packages('rcrossref', repo = \"https://cran.rstudio.com/\")"
 
-R --slave -e "install.packages('classyfireR', "https://cran.r-project.org/src/contrib/Archive/classyfireR/classyfireR_0.3.6.tar.gz", repo=NULL, type="source")"
+R --slave -e "install.packages(\"https://cran.r-project.org/src/contrib/Archive/classyfireR/classyfireR_0.3.6.tar.gz\", repo = NULL, type = "source")"
 
-R --slave -e "install.packages('ggstar', repo=\"https://cran.rstudio.com/\")"
+R --slave -e "install.packages('ggstar', repo = \"https://cran.rstudio.com/\")"
 
-R --slave -e "install.packages('ggtree', repo=\"https://bioconductor.org/packages/release/bioc/\")"
+R --slave -e "install.packages('ggtree', repo = \"https://bioconductor.org/packages/release/bioc/\")"
 
-R --slave -e "install.packages('ggtreeExtra', repo=\"https://bioconductor.org/packages/release/bioc/\")"
+R --slave -e "install.packages('ggtreeExtra', repo = \"https://bioconductor.org/packages/release/bioc/\")"
