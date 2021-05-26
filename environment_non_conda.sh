77cfb582551d2cd5b3c7f69f263d3538a80a5d64
Adafede <adriano.rutz@unige.ch>
2021-05-26 10:00:12 +0200

    update

--- environment_non_conda.sh
+++ environment_non_conda.sh
@@ -4,7 +4,7 @@ echo "Installing packages missing from conda"
 
 R --slave -e "install.packages('rcrossref', repo = \"https://cran.rstudio.com/\")"
 
-R --slave -e "install.packages(\"https://cran.r-project.org/src/contrib/Archive/classyfireR/classyfireR_0.3.6.tar.gz\", repo = NULL, type = "source")"
+R --slave -e "install.packages('https://cran.r-project.org/src/contrib/Archive/classyfireR/classyfireR_0.3.6.tar.gz', repo = NULL, type = 'source')"
 
 R --slave -e "install.packages('ggstar', repo = \"https://cran.rstudio.com/\")"
 
