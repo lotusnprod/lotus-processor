2d453ebdb11ba100dad1e0e51175b213e857959a
Adafede <adriano.rutz@unige.ch>
2021-05-26 08:28:11 +0200

    update(environment): update R package repositories in environment_non_conda.sh

--- environment_non_conda.sh
+++ environment_non_conda.sh
@@ -4,12 +4,10 @@ echo "Installing packages missing from conda"
 
 R --slave -e "install.packages('rcrossref', repo=\"https://cran.rstudio.com/\")"
 
-R --slave -e "install.packages('classyfireR', repo=\"https://cran.rstudio.com/\")"
+R --slave -e "install.packages('classyfireR', repo=\"https://github.com/aberHRML/classyfireR\")"
 
 R --slave -e "install.packages('ggstar', repo=\"https://cran.rstudio.com/\")"
 
-R --slave -e "install.packages('ggtree', repo=\"https://bioconductor.org/\")"
+R --slave -e "install.packages('ggtree', repo=\"https://bioconductor.org/packages/\")"
 
-R --slave -e "install.packages('ggtreeExtra', repo=\"https://bioconductor.org/\")"
-
-R --slave -e "install.packages('treeio', repo=\"https://bioconductor.org/\")"
+R --slave -e "install.packages('ggtreeExtra', repo=\"https://bioconductor.org/packages/\")"
