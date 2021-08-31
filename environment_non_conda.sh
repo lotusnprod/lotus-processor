a55b6f5204b663d77ae562e515d7adbf49872d88
Adafede <adriano.rutz@unige.ch>
2021-08-31 11:18:07 +0200

    [dependencies] readr 2.0 not available on conda (for now)

--- environment_non_conda.sh
+++ environment_non_conda.sh
@@ -4,6 +4,8 @@ echo "Installing packages missing from conda"
 
 R --slave -e "install.packages('rcrossref', repo = \"https://cran.rstudio.com/\")"
 
+R --slave -e "install.packages('readr', repo = \"https://cran.rstudio.com/\")"
+
 R --slave -e "install.packages('tidyjson', repo = \"https://cran.rstudio.com/\")"
 
 R --slave -e "install.packages('https://cran.r-project.org/src/contrib/Archive/classyfireR/classyfireR_0.3.6.tar.gz', repo = NULL, type = 'source')"
