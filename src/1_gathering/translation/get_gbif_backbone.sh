a4c6a113f98607dd993beea5c7bce295f0cb5fd9
Adafede <adriano.rutz@unige.ch>
2021-09-13 11:03:13 +0200

    adding headers

--- src/1_gathering/translation/get_gbif_backbone.sh
+++ src/1_gathering/translation/get_gbif_backbone.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
 # wget "https://hosted-datasets.gbif.org/datasets/backbone/backbone-current.zip" -O ../data/external/translationSource/common/backbone-current.zip
 
 wget "https://hosted-datasets.gbif.org/datasets/backbone/2021-03-03/backbone.zip" -O ../data/external/translationSource/common/backbone-current.zip
