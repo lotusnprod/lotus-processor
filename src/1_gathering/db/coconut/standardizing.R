5e470a19a395a927951ad99e2c972ab6737f586d
Adafede <adriano.rutz@unige.ch>
2021-09-15 07:36:44 +0200

    feat(auth): standardize coconut database processing

--- src/1_gathering/db/coconut/standardizing.R
+++ src/1_gathering/db/coconut/standardizing.R
@@ -9,6 +9,7 @@ library(dplyr)
 library(readr)
 library(splitstackshape)
 library(stringr)
+library(tidyr)
 
 # get paths
 database <- databases$get("coconut")
@@ -27,26 +28,146 @@ data_selected <- data_original %>%
     inchi = inchi,
     smiles = SMILES,
     biologicalsource = textTaxa,
-    reference_doi = citationDOI,
+    reference = citationDOI,
     reference_external = found_in_databases
   ) %>%
   filter(biologicalsource != "[notax]") %>%
   filter(biologicalsource != "[]") %>%
-  filter(reference_doi != "[]") %>%
+  filter(reference != "[]") %>%
   mutate(
     biologicalsource = gsub("\\[", "", biologicalsource),
     biologicalsource = gsub("\\]", "", biologicalsource),
     biologicalsource = gsub("\"", "", biologicalsource),
-    reference_doi = gsub("\\[", "", reference_doi),
-    reference_doi = gsub("\\]", "", reference_doi),
-    reference_doi = gsub("\"", "", reference_doi),
+    reference = gsub("\\[", "", reference),
+    reference = gsub("\\]", "", reference),
+    reference = gsub("\"", "", reference),
     # reference_external = gsub("\\[", "", reference_external),
     # reference_external = gsub("\\]", "", reference_external),
     # reference_external = gsub("\"", "", reference_external)
+  ) 
+
+data_counted <- data_selected %>%
+  rowwise() %>%
+  mutate(
+    nrefs = str_count(string = reference, pattern = ", "),
+    norganisms = str_count(string = biologicalsource, pattern = ", ")
+  )
+
+## right organism, right ref
+data_nosplit <- data_counted %>%
+  filter(nrefs == 0)
+
+## right organism, right ref
+data_easy <- data_counted %>%
+  filter(nrefs != 0 & nrefs == norganisms)
+
+## combination of all organisms vs all references since we do not know which one is good
+data_hard <- data_counted %>%
+  filter(nrefs != 0 & nrefs != norganisms)
+
+data_hard_harborne <- data_hard %>%
+  filter(
+    grepl(pattern = "Harborne, The Handbook of Natural Flavonoids, [0-9], \\([0-9]{4}\\), [0-9]{1-3}",
+          x = reference)
+  )
+
+data_hard_harborne_treated <- data_hard_harborne %>%
+  mutate(
+    reference = gsub(
+      pattern = "Harborne, The Handbook of Natural Flavonoids, [0-9], \\([0-9]{4}\\), [0-9]{1-3}",
+      replacement = "",
+      x = reference
+    )
   ) %>%
   cSplit(
     "biologicalsource",
-    sep = ",",
+    sep = ", ",
+    direction = "long",
+    fixed = TRUE
+  ) %>%
+  group_by(smiles) %>%
+  add_count() %>%
+  ungroup() %>%
+  filter(n == 1 |
+           biologicalsource %ni% c("plants", "marine", "fungi")) %>%
+  select(-n) %>%
+  cSplit("reference",
+         sep = ", ",
+         direction = "long",
+         fixed = TRUE) %>%
+  filter(!grepl(pattern = "^,", x = reference)) %>%
+  filter(!grepl(pattern = "^\\.", x = reference)) %>%
+  filter(!grepl(pattern = "^[0-9]{1-3},", x = reference)) %>%
+  filter(!grepl(pattern = "^\\([0-9]{4}\\)", x = reference)) %>%
+  filter(str_length(reference) >= 8) %>%
+  add_count(reference) %>%
+  filter(
+    n <= 5400 |
+      grepl(pattern = "Molecules_2015;20(8):15330-42",
+            x = reference) |
+      grepl(pattern = "Lu,Phytochem.,59,(2002),117",
+            x = reference)
+  ) %>%
+  select(-nrefs, -norganisms, -n) %>%
+  data.frame()
+
+data_hard_2 <- data_hard %>%
+  filter(
+    !grepl(pattern = "Harborne, The Handbook of Natural Flavonoids, [0-9], \\([0-9]{4}\\), [0-9]{1-3}",
+           x = reference)
+  )
+
+data_nosplit_treated <- data_nosplit %>%
+  cSplit(
+    "biologicalsource",
+    sep = ", ",
+    direction = "long",
+    fixed = TRUE
+  ) %>%
+  group_by(smiles) %>%
+  add_count() %>%
+  ungroup() %>%
+  filter(n == 1 |
+           biologicalsource %ni% c("plants", "marine", "fungi")) %>%
+  select(-n) %>%
+  cSplit("reference",
+         sep = ", ",
+         direction = "long",
+         fixed = TRUE) %>%
+  filter(str_length(reference) >= 8) %>%
+  select(-nrefs, -norganisms) %>%
+  data.frame()
+
+data_easy_treated <- data_easy %>%
+  cSplit("biologicalsource",
+         sep = ", ",
+         fixed = TRUE) %>%
+  cSplit("reference",
+         sep = ", ",
+         fixed = TRUE) %>%
+  pivot_longer(
+    cols = 7:(ncol(.)),
+    names_to = c("type", "number"),
+    names_sep = "_"
+  ) %>%
+  filter(!is.na(value)) %>%
+  pivot_wider(names_from = "type", values_from = "value") %>%
+  group_by(inchi) %>%
+  fill(reference, .direction = "downup") %>%
+  group_by(inchi) %>%
+  add_count() %>%
+  ungroup() %>%
+  filter(n == 1 |
+           biologicalsource %ni% c("plants", "marine", "fungi")) %>%
+  select(-n) %>%
+  filter(str_length(reference) >= 8) %>%
+  select(-nrefs, -norganisms, -number) %>%
+  data.frame()
+
+data_hard_treated <- data_hard_2 %>%
+  cSplit(
+    "biologicalsource",
+    sep = ", ",
     direction = "long",
     fixed = TRUE
   ) %>%
@@ -56,29 +177,38 @@ data_selected <- data_original %>%
   filter(n == 1 | biologicalsource %ni% c("plants","marine","fungi")) %>%
   select(-n) %>%
   cSplit(
-    "reference_doi",
+    "reference",
     sep = ", ",
     direction = "long",
     fixed = TRUE
   ) %>%
-  filter(str_length(reference_doi) >= 8) %>%
+  filter(str_length(reference) >= 8) %>%
+  add_count(reference) %>%
+  filter(n <= 15000) %>%
+  select(-nrefs, -norganisms, -n) %>%
   data.frame()
 
-data_corrected <- data_selected %>%
+data_treated <- rbind(data_nosplit_treated, 
+                      data_easy_treated,
+                      data_hard_harborne_treated,
+                      data_hard_treated) %>%
+  filter(grepl(pattern = "[0-9]",x = reference))
+
+data_corrected <- data_treated %>%
   mutate(
-    reference_publishingDetails = reference_doi,
-    reference_authors = reference_doi
+    reference_publishingDetails = reference,
+    reference_authors = reference
   ) %>%
-  mutate(n = nchar(reference_doi)) %>%
+  mutate(n = nchar(reference)) %>%
   mutate(
     reference_pubmed = ifelse(
       test = n == 8,
-      yes = str_extract(string = reference_doi, pattern = "[0-9]{8}"),
+      yes = str_extract(string = reference, pattern = "[0-9]{8}"),
       no = NA
     ),
     reference_doi = ifelse(
       test = n >= 9,
-      yes = str_extract(string = reference_doi, pattern = "^10.*"),
+      yes = str_extract(string = reference, pattern = "^10.*"),
       no = NA
     ),
     reference_publishingDetails = ifelse(
@@ -148,8 +278,8 @@ findCapitals_3 <- full_join(findCapitals_1, findCapitals_2) %>%
   filter(capitals_x > capitals_y)
 
 data_corrected_capitals <- left_join(data_corrected,
-  findCapitals_3,
-  by = c("biologicalsource" = "x")
+                                     findCapitals_3,
+                                     by = c("biologicalsource" = "x")
 ) %>%
   mutate(organism_clean = ifelse(
     test = !is.na(y),
@@ -166,12 +296,12 @@ data_corrected_capitals <- left_join(data_corrected,
   ) %>%
   mutate(n = str_count(organism_clean, "\\S+")) %>%
   mutate(organism_dirty = ifelse(test = n == 1,
-    yes = organism_clean,
-    no = NA
+                                 yes = organism_clean,
+                                 no = NA
   )) %>%
   mutate(organism_clean = ifelse(test = n > 1,
-    yes = organism_clean,
-    no = NA
+                                 yes = organism_clean,
+                                 no = NA
   )) %>%
   select(
     structure_inchi = inchi,
