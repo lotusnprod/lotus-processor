814f442877ee4aa71ab7e980760eea79f154aa26
Adriano Rutz <adriano.rutz@hotmail.com>
2021-09-16 14:00:20 +0000

    Update getOTL.sh

--- src/getOTL.sh
+++ src/getOTL.sh
@@ -8,7 +8,7 @@ if [ ! -f ../data/external/taxonomySource/organism/ott"$OTT_VERSION".tgz ]; then
   curl -o ../data/external/taxonomySource/organism/ott"$OTT_VERSION".tgz https://files.opentreeoflife.org/ott/ott"$OTT_VERSION"/ott"$OTT_VERSION".tgz
 fi
 
-if [ ! -f taxonomy.tsv ]; then
+if [ ! -f ott_taxonomy.tsv ]; then
   echo "Extracting"
   tar -xzf ../data/external/taxonomySource/organism/ott"$OTT_VERSION".tgz --strip-components=1 ott"$OTT_VERSION"/taxonomy.tsv
   sed "s/\t|\t/\t/g" <taxonomy.tsv | sed "s/\t$//g" >../data/external/taxonomySource/organism/taxonomy.tsv
@@ -16,6 +16,6 @@ if [ ! -f taxonomy.tsv ]; then
 fi
 
 echo "You can now import in sqlite: "
-echo " create if not exist table taxa_otl(uid int, parent_uid int, name text, rank text, sourceinfo text, uniqname text, flags text);"
+echo " create if not exist table ott_taxonomy(uid int, parent_uid int, name text, rank text, sourceinfo text, uniqname text, flags text);"
 echo " .separator \"\\t\""
-echo " .import taxonomy.tsv taxa_otl"
+echo " .import ott_taxonomy.tsv ott_taxonomy"
