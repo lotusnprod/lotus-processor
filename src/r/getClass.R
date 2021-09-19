9fb756d6cebe977d5a4b421def1320d81b6fcc71
Adafede <adriano.rutz@unige.ch>
2021-09-19 22:52:34 +0200

    fix

--- src/r/getClass.R
+++ src/r/getClass.R
@@ -12,49 +12,50 @@ library(purrr)
 #'
 #' @examples
 getClass <- function(X) {
-  tryCatch(
-    {
-      url_id <- paste0(url, order, queries[X])
-      result <- read_html(url_id) %>%
-        html_text() %>%
-        fromJSON()
-
-      df <- data.frame(
-        structure_smiles_2D = new$structure_smiles_2D[[X]],
-        query = new$query[[X]],
-        pathway = ifelse(
-          test = is_empty(result$pathway_results),
-          yes = NA,
-          no = result$pathway_results
-        ),
-        superclass = ifelse(
-          test = is_empty(result$superclass_results),
-          yes = NA,
-          no = result$superclass_results
-        ),
-        class = ifelse(
-          test = is_empty(result$class_results),
-          yes = NA,
-          no = result$class_results
-        ),
-        glycoside = ifelse(
-          test = is_empty(result$isglycoside),
-          yes = NA,
-          no = result$isglycoside
-        )
-      )
-      return(df)
-    },
-    error = function(e) {
-      df <- data.frame(
-        structure_smiles_2D = new$structure_smiles_2D[[X]],
-        query = new$query[[X]],
-        pathway = NA,
-        superclass = NA,
-        class = NA,
-        glycoside = NA
-      )
-      return(df)
-    }
-  )
+  tryCatch({
+    url_id <- paste0(url, order, queries[X])
+    result <- read_html(url_id) %>%
+      html_text() %>%
+      fromJSON()
+    
+    df <- data.frame(
+      structure_smiles_2D = new$structure_smiles_2D[[X]],
+      query = new$query[[X]],
+      pathway = if (!is_empty(result$pathway_results)) {
+        result$pathway_results
+      } else{
+        NA
+      },
+      superclass = if (!is_empty(result$superclass_results)) {
+        result$superclass_results
+      } else{
+        NA
+      }
+      ,
+      class = if (!is_empty(result$class_results)) {
+        result$class_results
+      } else{
+        NA
+      }
+      ,
+      glycoside = if (!is_empty(result$isglycoside)) {
+        result$isglycoside
+      } else{
+        NA
+      }
+    )
+    
+    return(df)
+  },
+  error = function(e) {
+    df <- data.frame(
+      structure_smiles_2D = new$structure_smiles_2D[[X]],
+      query = new$query[[X]],
+      pathway = NA,
+      superclass = NA,
+      class = NA,
+      glycoside = NA
+    )
+    return(df)
+  })
 }
