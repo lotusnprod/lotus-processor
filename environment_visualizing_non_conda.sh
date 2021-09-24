b7b6186a1802c3b656a80fa68822a90380087b6c
Adafede <adriano.rutz@unige.ch>
2021-09-24 09:02:37 +0200

    trying missing packages installation

--- environment_visualizing_non_conda.sh
+++ environment_visualizing_non_conda.sh
@@ -2,8 +2,6 @@
 
 echo "Installing packages missing from conda"
 
-R --slave -e "install.packages('ggstar', repo = \"https://cran.rstudio.com/\")"
+# R --slave -e "install.packages('ggstar', repo = \"https://cran.rstudio.com/\")"
 
-R --slave -e "install.packages('ggtree', repo = \"https://bioconductor.org/packages/release/bioc/\")"
-
-R --slave -e "install.packages('ggtreeExtra', repo = \"https://bioconductor.org/packages/release/bioc/\")"
+conda skeleton cran ggstar
