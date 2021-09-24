063ca7d98a294ed9e94513e41f43b3fed6e33bac
Adafede <adriano.rutz@unige.ch>
2021-09-24 18:22:54 +0200

    [fix] trying with other tar arguments

--- src/1_gathering/db/drduke/downloading.sh
+++ src/1_gathering/db/drduke/downloading.sh
@@ -7,7 +7,7 @@ mkdir -p ../data/external/dbSource/drduke/Duke-Source-CSV
 
 wget "https://data.nal.usda.gov/system/files/Duke-Source-CSV.zip" -O ../data/external/dbSource/drduke/Duke-Source-CSV.zip
 
-tar -xf ../data/external/dbSource/drduke/Duke-Source-CSV.zip -C ../data/external/dbSource/drduke/Duke-Source-CSV
+tar -xvzf ../data/external/dbSource/drduke/Duke-Source-CSV.zip -C ../data/external/dbSource/drduke/Duke-Source-CSV
 
 rm ../data/external/dbSource/drduke/Duke-Source-CSV.zip
 
