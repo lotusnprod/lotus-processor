373126beb17770ccf5666fcb98a668f5b8427ae0
Adafede <adriano.rutz@unige.ch>
2021-10-07 08:54:45 +0200

    refactor(environment_non_conda.sh): update conda environment setup to use conda run commands

--- environment_non_conda.sh
+++ environment_non_conda.sh
@@ -6,6 +6,6 @@ echo "Installing packages missing from conda"
 
 # R --slave -e "install.packages('https://cran.r-project.org/src/contrib/Archive/classyfireR/classyfireR_0.3.6.tar.gz', repo = NULL, type = 'source')"
 
-conda skeleton cran tidyjson
-conda skeleton cran classyfirer --allow-archived
-conda skeleton cran https://github.com/ropensci/rcrossref
+conda run -n lotus_env skeleton cran tidyjson
+conda run -n lotus_env skeleton cran classyfirer --allow-archived
+conda run -n lotus_env skeleton cran https://github.com/ropensci/rcrossref
