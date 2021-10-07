1685f2d989f7808c8e82f9f9b804aea704d16b02
Adafede <adriano.rutz@unige.ch>
2021-10-07 09:14:51 +0200

    Update environment_non_conda.sh

--- environment_non_conda.sh
+++ environment_non_conda.sh
@@ -2,10 +2,6 @@
 
 echo "Installing packages missing from conda"
 
-# R --slave -e "install.packages('tidyjson', repo = \"https://cran.rstudio.com/\")"
-
-# R --slave -e "install.packages('https://cran.r-project.org/src/contrib/Archive/classyfireR/classyfireR_0.3.6.tar.gz', repo = NULL, type = 'source')"
-
-conda run -n lotus_env skeleton cran tidyjson
-conda run -n lotus_env skeleton cran classyfirer --allow-archived
-conda run -n lotus_env skeleton cran https://github.com/ropensci/rcrossref
+conda skeleton cran tidyjson --output-dir $CONDA/envs/lotus_env/lib/R/library
+conda skeleton cran classyfirer --allow-archived --output-dir $CONDA/envs/lotus_env/lib/R/library
+conda skeleton cran https://github.com/ropensci/rcrossref --output-dir $CONDA/envs/lotus_env/lib/R/library
