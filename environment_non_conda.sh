55232f175721e22ae7ca2fb766f45fadf0613fa3
Adafede <adriano.rutz@unige.ch>
2021-10-07 10:24:34 +0200

    ci: update ci-processing.yml to use conda run and add .Renviron file creation

--- environment_non_conda.sh
+++ environment_non_conda.sh
@@ -2,6 +2,4 @@
 
 echo "Installing packages missing from conda"
 
-conda skeleton cran tidyjson --output-dir $CONDA/envs/lotus_env/lib/R/library
-conda skeleton cran classyfirer --allow-archived --output-dir $CONDA/envs/lotus_env/lib/R/library
-conda skeleton cran https://github.com/ropensci/rcrossref --output-dir $CONDA/envs/lotus_env/lib/R/library
+conda skeleton cran classyfirer --allow-archived
