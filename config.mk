b878b3010054f49dc89c919b8e78bb463729be69
Adafede <adriano.rutz@unige.ch>
2021-10-18 06:43:36 +0200

    feat(config.mk): update GNfinder and GNverifier versions

--- config.mk
+++ config.mk
@@ -8,8 +8,8 @@ export SRC_PATH ?= ${PWD}/src
 export BIN_PATH ?= ${PWD}/bin
 export TESTS_PATH ?= ${PWD}/tests
 
-export GNFINDER_VERSION = v0.15.1
-export GNVERIFIER_VERSION = v0.3.3
+export GNFINDER_VERSION = v0.16.1
+export GNVERIFIER_VERSION = v0.4.0
 export OPSIN_VERSION = 2.5.0
 
 export OTT_VERSION = 3.3
