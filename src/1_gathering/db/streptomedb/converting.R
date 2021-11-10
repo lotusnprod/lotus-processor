fed6501690047abf496a9442c0fe9c3966dab961
Adafede <adriano.rutz@unige.ch>
2021-11-10 14:33:10 +0100

    refactor(env): remove ChemmineR dependency from environment.yml and converting scripts

--- src/1_gathering/db/streptomedb/converting.R
+++ src/1_gathering/db/streptomedb/converting.R
@@ -4,7 +4,14 @@
 source("paths.R")
 source("r/standardizing_original.R")
 
-library(ChemmineR)
+if(!require(BiocManager)) {
+  install.packages("BiocManager")
+  library(BiocManager)
+}
+if(!require(ChemmineR)) {
+  BiocManager::install("BiocManager")
+  library(ChemmineR)
+}
 
 # get paths
 database <- databases$get("streptomedb")
