3a9a6d241532e3e076fc296d505bf3c7c83d3aab
Adafede <adriano.rutz@unige.ch>
2021-11-11 16:38:58 +0100

    test(integration_test.sh): update package dependencies and install GitHub repository

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-Rscript -e "remotes::install_github('ropensci/rcrossref')" &&
-make MODE=test lotus-quick &&
-cd src &&
-Rscript 1_gathering/db/manual/standardizing.R ../data/example/manual_example.tsv &&
-cd .. &&
+Rscript -e "
+if (!require(remotes)) {
+  install.packages(remotes)
+  require(
+    package = remotes,
+    quietly = TRUE
+  )
+}
+remotes::install_github('ropensci/rcrossref')
+"
+make MODE=test lotus-quick
+cd src
+Rscript 1_gathering/db/manual/standardizing.R ../data/example/manual_example.tsv
+cd ..
 make MODE=manual manual-entry
