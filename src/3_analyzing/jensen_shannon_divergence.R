4139cc109d3bc5f9005941db57e4ff7666fc1251
Adafede <adriano.rutz@unige.ch>
2021-11-29 16:10:03 +0100

    splitting function

--- src/3_analyzing/jensen_shannon_divergence.R
+++ src/3_analyzing/jensen_shannon_divergence.R
@@ -8,6 +8,7 @@ start <- Sys.time()
 log_debug("sourcing ...")
 log_debug("... paths")
 source("paths.R")
+source("r/get_jsd.R")
 
 log_debug("... libraries")
 library(data.table)
@@ -113,61 +114,6 @@ table_counted <- table |>
     structure_taxonomy_npclassifier_02superclass,
     structure_taxonomy_npclassifier_03class
   )
-
-#' Title
-#'
-#' @param X
-#'
-#' @return
-#' @export
-#'
-#' @examples
-get_jsd <- function(X) {
-  table_real <- table_counted |>
-    filter(!is.na(!!as.name(chem_level))) |>
-    distinct(
-      !!as.name(bio_level),
-      !!as.name(chem_level),
-      !!as.name(paste(chem_level, bio_level,
-        sep =
-          "_"
-      ))
-    ) |>
-    filter(!!as.name(chem_level) == Y[X]) |>
-    right_join(table |> distinct(!!as.name(bio_level))) |>
-    distinct(!!as.name(bio_level), !!as.name(paste(chem_level, bio_level,
-      sep =
-        "_"
-    ))) |>
-    filter(!is.na(!!as.name(bio_level))) |>
-    mutate_all(~ replace(., is.na(.), 0))
-
-  table_random <- table |>
-    filter(!is.na(!!as.name(bio_level))) |>
-    distinct(!!as.name(bio_level)) |>
-    cbind(1) ## whatever numbre since distribution
-
-  P <- table_real[[2]]
-  Q <- table_random[[2]]
-
-  x <- rbind(
-    P,
-    Q
-  )
-
-  jsd <- JSD(x, est.prob = "empirical")
-
-  result <- data.frame(
-    Y[X],
-    jsd
-  ) |>
-    rename(
-      !!as.name(chem_level) := Y.X.,
-      !!as.name(bio_level) := jsd
-    )
-
-  return(result)
-}
 
 log_debug("computing JSD at the chemical class level ...")
 chem_level <- "structure_taxonomy_npclassifier_03class"
