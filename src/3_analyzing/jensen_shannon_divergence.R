4a2d3ae9df30223cbffac4e861895eaa6c195500
Adafede <adriano.rutz@unige.ch>
2021-12-15 08:13:17 +0100

    [compatibility] replacing with old pipes

--- src/3_analyzing/jensen_shannon_divergence.R
+++ src/3_analyzing/jensen_shannon_divergence.R
@@ -23,7 +23,7 @@ log_debug("loading the LOTUS, this may take a while")
 table <- read_csv(file = file.path(
   pathDataProcessed,
   pathLastFrozen
-)) |>
+)) %>%
   select(
     structure_smiles_2D,
     structure_taxonomy_npclassifier_01pathway,
@@ -41,57 +41,57 @@ table <- read_csv(file = file.path(
     organism_taxonomy_08genus,
     organism_taxonomy_09species,
     organism_taxonomy_10varietas
-  ) |>
+  ) %>%
   distinct()
 
 log_debug("counting occurrences")
-table_counted <- table |>
+table_counted <- table %>%
   group_by(
     structure_taxonomy_npclassifier_01pathway,
     organism_taxonomy_02kingdom
-  ) |>
-  add_count(name = "structure_taxonomy_npclassifier_01pathway_organism_taxonomy_02kingdom") |>
+  ) %>%
+  add_count(name = "structure_taxonomy_npclassifier_01pathway_organism_taxonomy_02kingdom") %>%
   group_by(
     structure_taxonomy_npclassifier_02superclass,
     organism_taxonomy_02kingdom
-  ) |>
-  add_count(name = "structure_taxonomy_npclassifier_02superclass_organism_taxonomy_02kingdom") |>
+  ) %>%
+  add_count(name = "structure_taxonomy_npclassifier_02superclass_organism_taxonomy_02kingdom") %>%
   group_by(
     structure_taxonomy_npclassifier_03class,
     organism_taxonomy_02kingdom
-  ) |>
-  add_count(name = "structure_taxonomy_npclassifier_03class_organism_taxonomy_02kingdom") |>
+  ) %>%
+  add_count(name = "structure_taxonomy_npclassifier_03class_organism_taxonomy_02kingdom") %>%
   group_by(
     structure_taxonomy_npclassifier_01pathway,
     organism_taxonomy_06family
-  ) |>
-  add_count(name = "structure_taxonomy_npclassifier_01pathway_organism_taxonomy_06family") |>
+  ) %>%
+  add_count(name = "structure_taxonomy_npclassifier_01pathway_organism_taxonomy_06family") %>%
   group_by(
     structure_taxonomy_npclassifier_02superclass,
     organism_taxonomy_06family
-  ) |>
-  add_count(name = "structure_taxonomy_npclassifier_02superclass_organism_taxonomy_06family") |>
+  ) %>%
+  add_count(name = "structure_taxonomy_npclassifier_02superclass_organism_taxonomy_06family") %>%
   group_by(
     structure_taxonomy_npclassifier_03class,
     organism_taxonomy_06family
-  ) |>
-  add_count(name = "structure_taxonomy_npclassifier_03class_organism_taxonomy_06family") |>
+  ) %>%
+  add_count(name = "structure_taxonomy_npclassifier_03class_organism_taxonomy_06family") %>%
   group_by(
     structure_taxonomy_npclassifier_01pathway,
     organism_taxonomy_08genus
-  ) |>
-  add_count(name = "structure_taxonomy_npclassifier_01pathway_organism_taxonomy_08genus") |>
+  ) %>%
+  add_count(name = "structure_taxonomy_npclassifier_01pathway_organism_taxonomy_08genus") %>%
   group_by(
     structure_taxonomy_npclassifier_02superclass,
     organism_taxonomy_08genus
-  ) |>
-  add_count(name = "structure_taxonomy_npclassifier_02superclass_organism_taxonomy_08genus") |>
+  ) %>%
+  add_count(name = "structure_taxonomy_npclassifier_02superclass_organism_taxonomy_08genus") %>%
   group_by(
     structure_taxonomy_npclassifier_03class,
     organism_taxonomy_08genus
-  ) |>
-  add_count(name = "structure_taxonomy_npclassifier_03class_organism_taxonomy_08genus") |>
-  ungroup() |>
+  ) %>%
+  add_count(name = "structure_taxonomy_npclassifier_03class_organism_taxonomy_08genus") %>%
+  ungroup() %>%
   distinct(
     structure_taxonomy_npclassifier_01pathway,
     structure_taxonomy_npclassifier_02superclass,
@@ -108,7 +108,7 @@ table_counted <- table |>
     structure_taxonomy_npclassifier_01pathway_organism_taxonomy_08genus,
     structure_taxonomy_npclassifier_02superclass_organism_taxonomy_08genus,
     structure_taxonomy_npclassifier_03class_organism_taxonomy_08genus
-  ) |>
+  ) %>%
   arrange(
     structure_taxonomy_npclassifier_01pathway,
     structure_taxonomy_npclassifier_02superclass,
@@ -129,7 +129,7 @@ class_1 <- invisible(pbmclapply(
   FUN = get_jsd,
   X = X,
   mc.cores = min(max(1, parallel::detectCores() - 1), 10)
-)) |>
+)) %>%
   rbindlist()
 
 log_debug("... at the biological family level")
@@ -146,7 +146,7 @@ class_2 <- invisible(pbmclapply(
   ignore.interactive = TRUE,
   mc.style = "txt",
   mc.substyle = 1
-)) |>
+)) %>%
   rbindlist()
 
 log_debug("... at the biological kingdom level")
@@ -163,12 +163,12 @@ class_3 <- invisible(pbmclapply(
   ignore.interactive = TRUE,
   mc.style = "txt",
   mc.substyle = 1
-)) |>
+)) %>%
   rbindlist()
 
-class <- class_1 |>
-  full_join(class_2) |>
-  full_join(class_3) |>
+class <- class_1 %>%
+  full_join(class_2) %>%
+  full_join(class_3) %>%
   data.frame()
 
 colnames(class)[grepl(pattern = "organism", x = colnames(class))] <-
@@ -188,7 +188,7 @@ superclass_1 <- invisible(pbmclapply(
   FUN = get_jsd,
   X = X,
   mc.cores = min(max(1, parallel::detectCores() - 1), 10)
-)) |>
+)) %>%
   rbindlist()
 
 log_debug("... at the biological family level")
@@ -205,7 +205,7 @@ superclass_2 <- invisible(pbmclapply(
   ignore.interactive = TRUE,
   mc.style = "txt",
   mc.substyle = 1
-)) |>
+)) %>%
   rbindlist()
 
 log_debug("... at the biological kingdom level")
@@ -222,12 +222,12 @@ superclass_3 <- invisible(pbmclapply(
   ignore.interactive = TRUE,
   mc.style = "txt",
   mc.substyle = 1
-)) |>
+)) %>%
   rbindlist()
 
-superclass <- superclass_1 |>
-  full_join(superclass_2) |>
-  full_join(superclass_3) |>
+superclass <- superclass_1 %>%
+  full_join(superclass_2) %>%
+  full_join(superclass_3) %>%
   data.frame()
 
 colnames(superclass)[grepl(pattern = "organism", x = colnames(superclass))] <-
@@ -247,7 +247,7 @@ pathway_1 <- invisible(pbmclapply(
   FUN = get_jsd,
   X = X,
   mc.cores = min(max(1, parallel::detectCores() - 1), 10)
-)) |>
+)) %>%
   rbindlist()
 
 log_debug("... at the biological family level")
@@ -264,7 +264,7 @@ pathway_2 <- invisible(pbmclapply(
   ignore.interactive = TRUE,
   mc.style = "txt",
   mc.substyle = 1
-)) |>
+)) %>%
   rbindlist()
 
 log_debug("... at the biological kingdom level")
@@ -281,61 +281,61 @@ pathway_3 <- invisible(pbmclapply(
   ignore.interactive = TRUE,
   mc.style = "txt",
   mc.substyle = 1
-)) |>
+)) %>%
   rbindlist()
 
-pathway <- pathway_1 |>
-  full_join(pathway_2) |>
-  full_join(pathway_3) |>
+pathway <- pathway_1 %>%
+  full_join(pathway_2) %>%
+  full_join(pathway_3) %>%
   data.frame()
 
 colnames(pathway)[grepl(pattern = "organism", x = colnames(pathway))] <-
   paste(colnames(pathway)[grepl(pattern = "organism", x = colnames(pathway))], "JSD", sep = "_")
 
 log_debug("pivoting results")
-class_pivoted <- class |>
-  pivot_longer(cols = 2:4, values_to = c("value_score")) |>
+class_pivoted <- class %>%
+  pivot_longer(cols = 2:4, values_to = c("value_score")) %>%
   mutate(
     name_structure = "structure_taxonomy_npclassifier_03class",
     value_structure = structure_taxonomy_npclassifier_03class,
     name_score = name
-  ) |>
+  ) %>%
   select(
     name_structure,
     value_structure,
     name_score,
     value_score
-  ) |>
+  ) %>%
   filter(!is.na(value_score))
 
-superclass_pivoted <- superclass |>
-  pivot_longer(cols = 2:4, values_to = c("value_score")) |>
+superclass_pivoted <- superclass %>%
+  pivot_longer(cols = 2:4, values_to = c("value_score")) %>%
   mutate(
     name_structure = "structure_taxonomy_npclassifier_02superclass",
     value_structure = structure_taxonomy_npclassifier_02superclass,
     name_score = name
-  ) |>
+  ) %>%
   select(
     name_structure,
     value_structure,
     name_score,
     value_score
-  ) |>
+  ) %>%
   filter(!is.na(value_score))
 
-pathway_pivoted <- pathway |>
-  pivot_longer(cols = 2:4, values_to = c("value_score")) |>
+pathway_pivoted <- pathway %>%
+  pivot_longer(cols = 2:4, values_to = c("value_score")) %>%
   mutate(
     name_structure = "structure_taxonomy_npclassifier_01pathway",
     value_structure = structure_taxonomy_npclassifier_01pathway,
     name_score = name
-  ) |>
+  ) %>%
   select(
     name_structure,
     value_structure,
     name_score,
     value_score
-  ) |>
+  ) %>%
   filter(!is.na(value_score))
 
 final <- rbind(pathway_pivoted, superclass_pivoted, class_pivoted)
@@ -344,25 +344,29 @@ log_debug("exporting")
 write_delim(
   x = final,
   file = file.path(pathDataProcessed, "jsd_full.tsv"),
-  delim = "\t"
+  delim = "\t",
+  na = ""
 )
 
 write_delim(
   x = class,
   file = file.path(pathDataProcessed, "jsd_class.tsv"),
-  delim = "\t"
+  delim = "\t",
+  na = ""
 )
 
 write_delim(
   x = superclass,
   file = file.path(pathDataProcessed, "jsd_superclass.tsv"),
-  delim = "\t"
+  delim = "\t",
+  na = ""
 )
 
 write_delim(
   x = pathway,
   file = file.path(pathDataProcessed, "jsd_pathway.tsv"),
-  delim = "\t"
+  delim = "\t",
+  na = ""
 )
 
 end <- Sys.time()
