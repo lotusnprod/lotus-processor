bb0fd5f27f49c9d55e18438e61da51652faccb84
Adafede <adriano.rutz@unige.ch>
2021-12-28 18:12:15 +0100

    GNVerifier update

--- config.mk
+++ config.mk
@@ -9,7 +9,7 @@ export BIN_PATH ?= ${PWD}/bin
 export TESTS_PATH ?= ${PWD}/tests
 
 export GNFINDER_VERSION = v0.16.3
-export GNVERIFIER_VERSION = v0.6.3
+export GNVERIFIER_VERSION = v0.6.4
 export OPSIN_VERSION = 2.5.0
 
 export OTT_VERSION = 3.3
