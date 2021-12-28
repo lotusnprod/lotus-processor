b916b0c9963be0ebc02ac45e8d2b91ceff55f414
Adafede <adriano.rutz@unige.ch>
2021-12-28 18:45:45 +0100

    adding taxa ranks translations for World Flora taxonomic DBs

--- src/1_gathering/dictionary/gathering_custom_dictionaries.sh
+++ src/1_gathering/dictionary/gathering_custom_dictionaries.sh
@@ -5,7 +5,7 @@ if [ ! -d ../data/external/dictionarySource/ ]; then
 
 mkdir -p ../data/external/dictionarySource/
 
-wget -nv "https://zenodo.org/record/5801816/files/dictionarySource.zip?download=1" -O ../data/external/dictionarySource.zip
+wget -nv "https://zenodo.org/record/5807826/files/dictionarySource.zip?download=1" -O ../data/external/dictionarySource.zip
 
 unzip ../data/external/dictionarySource.zip -d ../data/external/
 
