f3c209059b0ac856d2f68e1fb434d054016584a9
Adafede <adriano.rutz@hotmail.com>
2022-03-23 07:38:12 +0100

    gnfinder update

--- config.mk
+++ config.mk
@@ -8,7 +8,7 @@ export SRC_PATH ?= ${PWD}/src
 export BIN_PATH ?= ${PWD}/bin
 export TESTS_PATH ?= ${PWD}/tests
 
-export GNFINDER_VERSION = v0.18.0
+export GNFINDER_VERSION = v0.18.3
 export GNVERIFIER_VERSION = v0.9.0
 export OPSIN_VERSION = 2.6.0
 
