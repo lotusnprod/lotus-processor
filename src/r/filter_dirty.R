2fcc7b01311efc99b4bdefd2c9298302b60a6a98
Adafede <adriano.rutz@hotmail.com>
2022-05-14 07:53:59 +0200

    feat(filter_dirty): add coconut cleaning functionality

--- src/r/filter_dirty.R
+++ src/r/filter_dirty.R
@@ -73,7 +73,27 @@ filter_dirty <- function(dataframe) {
       ) &
         as.numeric(referenceCleaned_score_titleOrganism) == 1
     )
-
+  
+  #' additional coconut cleaning
+  dfSplit_1 <- dfSplit %>%
+    filter(database == "coconut")
+  
+  dfSplit_1_1 <- dfSplit %>%
+    filter(database == "coconut") %>%
+    rowwise() %>%
+    filter(grepl(pattern = organismValue, x = referenceCleanedTitle))
+  
+  dfSplit_1_2 <- dfSplit %>%
+    filter(database == "coconut") %>%
+    anti_join(dfSplit_1_1, by = c(
+      "structureValue" = "structureValue",
+      "referenceValue" = "referenceValue"))
+  
+  dfSplit_2 <- bind_rows(dfSplit_1_1, dfSplit_1_2)
+  
+  dfSplit_3 <- dfSplit %>%
+    filter(database != "coconut")
+  
   dfTitle <- dfRest %>%
     filter(referenceType == "title") %>%
     filter(as.numeric(referenceCleaned_score_distance) <= 10)
@@ -85,7 +105,8 @@ filter_dirty <- function(dataframe) {
       dfPubmed,
       dfTitle,
       dfOriginal,
-      dfSplit,
+      dfSplit_2,
+      dfSplit_3,
       dfPublishingDetails
     )
 
