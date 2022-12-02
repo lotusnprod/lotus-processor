450ca32f189a508d84643db390d3e70c46946325
Adriano Rutz <adriano.rutz@hotmail.com>
2022-12-02 14:03:02 +0100

    feat(db): standardize data structures and common names from various sources

--- src/1_gathering/translation/tcm.R
+++ src/1_gathering/translation/tcm.R
@@ -21,86 +21,89 @@ if (mode == "full") {
   ## files
   ### dictionary from TMMC
   database <- databases$get("tmmc")
-  tcmNamesDic_1 <- read_excel(database$sourceFiles$tsv,
-    sheet = 1
-  ) %>%
-    mutate_all(as.character) %>%
-    select(
+  tcmNamesDic_1 <-
+    readxl::read_excel(
+      path = database$sourceFiles$tsv,
+      sheet = 1
+    ) |>
+    dplyr::mutate_all(as.character) |>
+    dplyr::select(
       latin = LATIN,
       common = COMMON,
       biologicalsource = SCIENCE
-    ) %>%
-    distinct(biologicalsource, .keep_all = TRUE)
+    ) |>
+    dplyr::distinct(biologicalsource, .keep_all = TRUE)
 
   ### dictionary from TCMID
   if (file.exists(pathDataExternalTranslationSourceTcmTcmid)) {
     tcmNamesDic_2 <-
-      read_delim(file = pathDataExternalTranslationSourceTcmTcmid) %>%
-      select(
+      readr::read_delim(file = pathDataExternalTranslationSourceTcmTcmid) |>
+      dplyr::select(
         latin = `Latin Name`,
         common = `English Name`
-      ) %>%
-      filter(!is.na(common) &
-        !is.na(latin)) %>%
-      mutate(biologicalsource = latin)
+      ) |>
+      dplyr::filter(!is.na(common) &
+        !is.na(latin)) |>
+      dplyr::mutate(biologicalsource = latin)
   }
 
   ## dictionary from Chinese Medicine Board of Australia
   tcmNamesDic_3 <-
-    read_excel(
+    readxl::read_excel(
       path = pathDataExternalTranslationSourceTcmCmba,
       sheet = 1
     ) %>%
-    mutate_all(as.character) %>%
-    select(
+    dplyr::mutate_all(as.character) %>%
+    dplyr::select(
       latin = 6,
       common = 9
     ) %>%
-    mutate(biologicalsource = latin) %>%
-    filter(row.names(.) != 1) %>%
-    filter(common != "N/A" &
+    dplyr::mutate(biologicalsource = latin) %>%
+    dplyr::filter(row.names(.) != 1) %>%
+    dplyr::filter(common != "N/A" &
       latin != "N/A")
 
   # latin genitive dictionaries
   ## i
   latinGenitiveIDic <-
-    read_delim(
+    readr::read_delim(
       file = pathDataInterimDictionariesLatinGenitiveI,
-      delim = "\t",
-    ) %>%
-    mutate(n = str_count(string = genitive)) %>%
-    arrange(desc(n)) %>%
-    select(-n)
+      delim = "\t"
+    ) |>
+    dplyr::mutate(n = stringr::str_count(string = genitive)) |>
+    dplyr::arrange(dplyr::desc(n)) |>
+    dplyr::select(-n)
 
   ## is
   latinGenitiveIsDic <-
-    read_delim(
+    readr::read_delim(
       file = pathDataInterimDictionariesLatinGenitiveIs,
-      delim = "\t",
-    ) %>%
-    mutate(n = str_count(string = genitive)) %>%
-    arrange(desc(n)) %>%
-    select(-n)
+      delim = "\t"
+    ) |>
+    dplyr::mutate(n = stringr::str_count(string = genitive)) |>
+    dplyr::arrange(dplyr::desc(n)) %>%
+    dplyr::select(-n)
 
   ## parts
   latinGenitivePartsDic <-
-    read_delim(
+    readr::read_delim(
       file = pathDataInterimDictionariesLatinPlantParts,
-      delim = "\t",
+      delim = "\t"
     )
 
   ## manually subtracted entries
   manualSubtraction <-
-    read_delim(
+    readr::read_delim(
       file = pathDataInterimDictionariesTcmManualSubtraction,
-      delim = "\t",
+      delim = "\t"
     )
 
   ## forcing organisms in test file to be in sampled dic
-  test_organisms <- read_delim(
-    file = "../tests/tests_min.tsv",
-    delim = "\t"
-  )
+  test_organisms <-
+    readr::read_delim(
+      file = "../tests/tests_min.tsv",
+      delim = "\t"
+    )
 
   # cleaning
   ## tcm_1
@@ -124,54 +127,54 @@ if (mode == "full") {
 
   ## tcm_3
   tcmNamesDic_3 <- tcmNamesDic_3 %>%
-    cSplit(
+    splitstackshape::cSplit(
       splitCols = "biologicalsource",
       sep = "1. ",
       fixed = TRUE,
       stripWhite = FALSE
     ) %>%
-    select(common,
+    dplyr::select(common,
       biologicalsource = biologicalsource_2
     ) %>%
-    filter(!is.na(biologicalsource)) %>%
-    cSplit(
+    dplyr::filter(!is.na(biologicalsource)) %>%
+    splitstackshape::cSplit(
       splitCols = "biologicalsource",
       sep = "2. ",
       fixed = TRUE,
       stripWhite = FALSE
     ) %>%
-    cSplit(
+    splitstackshape::cSplit(
       splitCols = "biologicalsource_2",
       sep = "3. ",
       fixed = TRUE,
       stripWhite = FALSE
     ) %>%
-    cSplit(
+    splitstackshape::cSplit(
       splitCols = "biologicalsource_2_2",
       sep = "4. ",
       fixed = TRUE,
       stripWhite = FALSE
     ) %>%
-    cSplit(
+    splitstackshape::cSplit(
       splitCols = "biologicalsource_2_2_2",
       sep = "5. ",
       fixed = TRUE,
       stripWhite = FALSE
     ) %>%
-    cSplit(
+    splitstackshape::cSplit(
       splitCols = "biologicalsource_2_2_2_2",
       sep = "6. ",
       fixed = TRUE,
       stripWhite = FALSE
     ) %>%
-    cSplit(
+    splitstackshape::cSplit(
       splitCols = "biologicalsource_2_2_2_2_2",
       sep = "7. ",
       fixed = TRUE,
       stripWhite = FALSE
     ) %>%
-    group_by(rownames(.)) %>%
-    summarise(
+    dplyr::group_by(rownames(.)) %>%
+    dplyr::summarise(
       common = first(common),
       biologicalsource = paste(
         biologicalsource_1[!is.na(biologicalsource_1)],
@@ -183,8 +186,8 @@ if (mode == "full") {
         biologicalsource_2_2_2_2_2_2[!is.na(biologicalsource_2_2_2_2_2_2)],
         sep = " "
       )
-    ) %>%
-    select(common, biologicalsource)
+    ) |>
+    dplyr::select(common, biologicalsource)
 
   # capitalizing first letter only
   tcmNamesDic_1$latin <- tolower(x = tcmNamesDic_1$latin)
@@ -203,37 +206,37 @@ if (mode == "full") {
 
     # reordering tcm names (eg. not Radix gentianae but Gentianae radix)
     tcmNamesDic_2 <-
-      tcm_inverting(x = tcmNamesDic_2) %>%
-      mutate(latin = biologicalsource) %>%
-      select(
+      tcm_inverting(x = tcmNamesDic_2) |>
+      dplyr::mutate(latin = biologicalsource) |>
+      dplyr::select(
         latin,
         common,
         biologicalsource
-      ) %>%
-      filter(!is.na(biologicalsource)) %>%
-      distinct(biologicalsource, .keep_all = TRUE)
+      ) |>
+      dplyr::filter(!is.na(biologicalsource)) |>
+      dplyr::distinct(biologicalsource, .keep_all = TRUE)
 
     tcmNamesDic_2 <-
-      tcm_cleaning(x = tcmNamesDic_2) %>%
-      filter(!is.na(biologicalsource)) %>%
-      distinct(biologicalsource, .keep_all = TRUE) %>%
-      mutate(biologicalsource = latin) %>%
-      select(common, biologicalsource)
+      tcm_cleaning(x = tcmNamesDic_2) |>
+      dplyr::filter(!is.na(biologicalsource)) |>
+      dplyr::distinct(biologicalsource, .keep_all = TRUE) |>
+      dplyr::mutate(biologicalsource = latin) |>
+      dplyr::select(common, biologicalsource)
   }
 
   # cleaning most occurring biological parts from names (eg. radix, flos, folium, cortrex, ...)
   tcmNamesDic_1 <-
-    tcm_cleaning(x = tcmNamesDic_1) %>%
-    filter(!is.na(biologicalsource)) %>%
-    distinct(biologicalsource, .keep_all = TRUE)
+    tcm_cleaning(x = tcmNamesDic_1) |>
+    dplyr::filter(!is.na(biologicalsource)) |>
+    dplyr::distinct(biologicalsource, .keep_all = TRUE)
 
-  tcmNamesDic_1_1 <- tcmNamesDic_1 %>%
-    select(common = latin, biologicalsource) %>%
-    filter(!is.na(common))
+  tcmNamesDic_1_1 <- tcmNamesDic_1 |>
+    dplyr::select(common = latin, biologicalsource) |>
+    dplyr::filter(!is.na(common))
 
-  tcmNamesDic_1_2 <- tcmNamesDic_1 %>%
-    select(common, biologicalsource) %>%
-    filter(!is.na(common))
+  tcmNamesDic_1_2 <- tcmNamesDic_1 |>
+    dplyr::select(common, biologicalsource) |>
+    dplyr::filter(!is.na(common))
 
   # joining
   tcmNamesDic <-
@@ -244,30 +247,30 @@ if (mode == "full") {
         tcmNamesDic_2
       },
       tcmNamesDic_3
-    ) %>%
-    distinct(common, .keep_all = TRUE) %>%
-    cSplit(
+    ) |>
+    dplyr::distinct(common, .keep_all = TRUE) |>
+    splitstackshape::cSplit(
       splitCols = "biologicalsource",
       sep = " "
-    ) %>%
-    select(common,
+    ) |>
+    dplyr::select(common,
       biologicalsource = biologicalsource_01
     )
 
   # filtering empty entries
-  tcmNamesDic <- tcmNamesDic %>%
-    filter(biologicalsource != " NA ")
+  tcmNamesDic <- tcmNamesDic |>
+    dplyr::filter(biologicalsource != " NA ")
 
   # manual curation
   ## ae
-  tcmNamesDic_ae <- tcmNamesDic %>%
-    distinct(biologicalsource, .keep_all = TRUE) %>%
-    arrange(biologicalsource) %>%
-    filter(grepl(
+  tcmNamesDic_ae <- tcmNamesDic |>
+    dplyr::distinct(biologicalsource, .keep_all = TRUE) |>
+    dplyr::arrange(biologicalsource) |>
+    dplyr::filter(grepl(
       pattern = "\\w+ae\\b",
       x = biologicalsource
-    )) %>%
-    mutate(newbiologicalsource = biologicalsource)
+    )) |>
+    dplyr::mutate(newbiologicalsource = biologicalsource)
 
   tcmNamesDic_ae$newbiologicalsource <- gsub(
     pattern = ".{2}$",
@@ -276,20 +279,20 @@ if (mode == "full") {
   )
 
   ## i
-  tcmNamesDic_i <- tcmNamesDic %>%
-    distinct(biologicalsource, .keep_all = TRUE) %>%
-    arrange(biologicalsource) %>%
-    filter(grepl(
+  tcmNamesDic_i <- tcmNamesDic |>
+    dplyr::distinct(biologicalsource, .keep_all = TRUE) |>
+    dplyr::arrange(biologicalsource) |>
+    dplyr::filter(grepl(
       pattern = "\\w+i\\b",
       x = biologicalsource
-    )) %>%
-    mutate(newbiologicalsource = biologicalsource)
+    )) |>
+    dplyr::mutate(newbiologicalsource = biologicalsource)
 
   a <- paste0("\\b", latinGenitiveIDic$genitive, "\\b")
   b <- latinGenitiveIDic$nominative
 
   tcmNamesDic_i$newbiologicalsource <-
-    stri_replace_all_regex(
+    stringi::stri_replace_all_regex(
       str = tcmNamesDic_i$newbiologicalsource,
       pattern = a,
       replacement = b,
@@ -306,20 +309,20 @@ if (mode == "full") {
   # example Tadehagi or Thlaspi
 
   ## is
-  tcmNamesDic_is <- tcmNamesDic %>%
-    distinct(biologicalsource, .keep_all = TRUE) %>%
-    arrange(biologicalsource) %>%
-    filter(grepl(
+  tcmNamesDic_is <- tcmNamesDic |>
+    dplyr::distinct(biologicalsource, .keep_all = TRUE) |>
+    dplyr::arrange(biologicalsource) |>
+    dplyr::filter(grepl(
       pattern = "\\w+is\\b",
       x = biologicalsource
-    )) %>%
-    mutate(newbiologicalsource = biologicalsource)
+    )) |>
+    dplyr::mutate(newbiologicalsource = biologicalsource)
 
   c <- paste0("\\b", latinGenitiveIsDic$genitive, "\\b")
   d <- latinGenitiveIsDic$nominative
 
   tcmNamesDic_is$newbiologicalsource <-
-    stri_replace_all_regex(
+    stringi::stri_replace_all_regex(
       str = tcmNamesDic_is$newbiologicalsource,
       pattern = c,
       replacement = d,
@@ -331,14 +334,15 @@ if (mode == "full") {
   # example Agathis, Aleuritopteris, Amaryllis, ...
 
   # joining
-  tcmNamesDicCurated <- full_join(tcmNamesDic, tcmNamesDic_ae)
+  tcmNamesDicCurated <-
+    dplyr::full_join(tcmNamesDic, tcmNamesDic_ae)
 
   # removing some organism parts
   e <- paste0("\\b", latinGenitivePartsDic$part, "\\b")
   f <- as.character(latinGenitivePartsDic$replacement)
 
   tcmNamesDicCurated$newbiologicalsource <-
-    stri_replace_all_regex(
+    stringi::stri_replace_all_regex(
       str = tcmNamesDicCurated$newbiologicalsource,
       pattern = e,
       replacement = f,
@@ -354,26 +358,26 @@ if (mode == "full") {
 
   # joining
   tcmNamesDicCurated <-
-    rbind(tcmNamesDicCurated, tcmNamesDic_i, tcmNamesDic_is) %>%
-    arrange(newbiologicalsource) %>%
-    distinct(common, .keep_all = TRUE)
+    rbind(tcmNamesDicCurated, tcmNamesDic_i, tcmNamesDic_is) |>
+    dplyr::arrange(newbiologicalsource) |>
+    dplyr::distinct(common, .keep_all = TRUE)
 
   # deleting ambiguous entries
-  tcmNamesDicCurated <- tcmNamesDicCurated %>%
-    filter(!common %in% manualSubtraction$common)
+  tcmNamesDicCurated <- tcmNamesDicCurated |>
+    dplyr::filter(!common %in% manualSubtraction$common)
 
   # sorting in appropriate order
-  tcmNamesDicCurated <- tcmNamesDicCurated %>%
-    mutate(n = str_count(string = common)) %>%
-    arrange(desc(n)) %>%
+  tcmNamesDicCurated <- tcmNamesDicCurated |>
+    dplyr::mutate(n = stringr::str_count(string = common)) |>
+    dplyr::arrange(dplyr::desc(n)) |>
     # sorting for replacements like "sea cucumber" and so on...
-    select(
+    dplyr::select(
       vernacularName = common,
       canonicalName = biologicalsource,
       newCanonicalName = newbiologicalsource
-    ) %>%
-    distinct() %>%
-    filter(vernacularName != canonicalName |
+    ) |>
+    dplyr::distinct() |>
+    dplyr::filter(vernacularName != canonicalName |
       is.na(newCanonicalName))
 
   tcmNamesDicCurated$vernacularName <-
@@ -407,27 +411,27 @@ if (mode == "full") {
     kind = "Mersenne-Twister",
     normal.kind = "Inversion"
   )
-  tcmNamesDicCurated_sampled_1 <- tcmNamesDicCurated %>%
-    sample_n(500)
+  tcmNamesDicCurated_sampled_1 <- tcmNamesDicCurated |>
+    dplyr::sample_n(500)
 
-  tcmNamesDicCurated_sampled_2 <- tcmNamesDicCurated %>%
-    filter(grepl(
+  tcmNamesDicCurated_sampled_2 <- tcmNamesDicCurated |>
+    dplyr::filter(grepl(
       pattern = paste(test_organisms$organismValue, collapse = "|"),
       x = vernacularName,
       ignore.case = TRUE
     ))
 
   tcmNamesDicCurated_sampled <-
-    bind_rows(
+    dplyr::bind_rows(
       tcmNamesDicCurated_sampled_1,
       tcmNamesDicCurated_sampled_2
-    ) %>%
-    mutate(n = str_count(string = vernacularName)) %>%
-    arrange(desc(n)) %>%
-    select(-n)
+    ) |>
+    dplyr::mutate(n = stringr::str_count(string = vernacularName)) |>
+    dplyr::arrange(dplyr::desc(n)) |>
+    dplyr::select(-n)
 
   # exporting
-  write_delim(
+  readr::write_delim(
     x = tcmNamesDicCurated,
     delim = "\t",
     file = gzfile(
@@ -439,7 +443,7 @@ if (mode == "full") {
     escape = "double"
   )
 
-  write_delim(
+  readr::write_delim(
     x = tcmNamesDicCurated_sampled,
     delim = "\t",
     file = gzfile(
